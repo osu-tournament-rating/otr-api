9eee6248ba8b2aaf69a3d6ea60680190e9a7a880
hburn7 <hburnett777@gmail.com>
2023-08-03 21:41:23 -0400

    Add schema.sql

---
+++ scripts/schema.sql
@@ -0,0 +1,93 @@
+create table players
+(
+    id      integer   default nextval('"Player_id_seq"'::regclass) not null
+        constraint "Player_pk"
+            primary key,
+    osu_id  bigint                                                 not null
+        constraint "Players_osuid"
+            unique,
+    created timestamp default CURRENT_TIMESTAMP                    not null
+);
+
+create table users
+(
+    id         integer   default nextval('"User_id_seq"'::regclass) not null
+        constraint "User_pk"
+            primary key,
+    player_id  integer                                              not null
+        constraint "Users___fkplayerid"
+            references players,
+    last_login timestamp                                            not null,
+    created    timestamp default CURRENT_TIMESTAMP                  not null
+);
+
+create table matchdata
+(
+    id                  integer default nextval('"MatchData_id_seq"'::regclass) not null
+        constraint "MatchData_pk"
+            primary key,
+    player_id           integer                                                 not null
+        constraint "MatchData___fkplayerid"
+            references players,
+    osu_match_id        bigint                                                  not null,
+    game_id             integer                                                 not null,
+    scoring_type        text                                                    not null,
+    score               double precision                                        not null,
+    osu_beatmap_id      bigint                                                  not null,
+    game_raw_mods       integer,
+    raw_mods            integer                                                 not null,
+    match_name          text                                                    not null,
+    mode                text                                                    not null,
+    match_start_date    timestamp                                               not null,
+    created             timestamp                                               not null,
+    free_mod            boolean default false,
+    force_mod           boolean default false,
+    team_type           text,
+    team                text,
+    osu_name            text,
+    osu_rank            integer,
+    osu_badges          integer                                                 not null,
+    osu_duel_starrating double precision,
+    accuracy            double precision,
+    cs                  double precision,
+    ar                  double precision,
+    od                  double precision
+);
+
+create table ratings
+(
+    id            integer   default nextval('"Ratings_id_seq"'::regclass) not null
+        constraint "Ratings_pk"
+            primary key,
+    player_id     integer                                                 not null
+        constraint "Ratings___fkplayerid"
+            references players,
+    mu            double precision                                        not null,
+    sigma         double precision                                        not null,
+    mu_initial    double precision                                        not null,
+    sigma_initial double precision                                        not null,
+    created       timestamp default CURRENT_TIMESTAMP                     not null,
+    updated       timestamp                                               not null
+);
+
+create index "Ratings__mu"
+    on ratings (mu desc, mu desc);
+
+create table ratinghistories
+(
+    id        integer   default nextval('"RatingHistories_id_seq"'::regclass) not null
+        constraint "RatingHistories_pk"
+            primary key,
+    player_id integer                                                         not null
+        constraint "RatingHistories___fkplayerid"
+            references players,
+    mu        double precision                                                not null,
+    sigma     double precision                                                not null,
+    created   timestamp default CURRENT_TIMESTAMP                             not null
+);
+
+create table config
+(
+    key   text not null,
+    value text not null
+);
