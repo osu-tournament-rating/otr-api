9ef88531900988ba279ee5eac87fb4c524a231a4
hburn7 <hburnett777@gmail.com>
2023-08-24 10:49:35 -0400

    Added additional tests & fixed regex

--- API/Osu/LobbyNameChecker.cs
+++ API/Osu/LobbyNameChecker.cs
@@ -7,13 +7,11 @@ public static class LobbyNameChecker
 	// List of regex patterns for lobby names
 	private static readonly List<string> patterns = new()
 	{
-		@".+?:\s?\((.+?)\)\s?vs\.?\s?\((.+?)\)",
-		@"""[\w\s!:\-]+?:\s?([^()]+?)\s?vs\s?([^()]+?)""",
-		@"\([\w\s!:\-]+?\):\s?\(([^)]+)\)\s?vs\s?\(([^)]+)\)",
-		@"\([\w\s!:\-]+?:\s?([^)]+?)\)\s?vs\s?\(([^)]+)\)",
-		@".+?:\s?\(([^)]+)\)\s?vs\.\s?\(([^)]+)\)",
-		@".+?:\s?[^()]+?\s?vs\.?\s?[^()]+?$",
-		@".+?:.*?\((.+?)\)\s?vs\.?\s?\((.+?)\)"
+		@"^[\w\.""'-]+:(\s*.+\|)?\s*\([^\)]+\)\s+vs\.?\s+\([^\)]+\)$",
+		@"^[\w\.""'-]+:(\s*.+\|)?\s*\([^\)]+\)\s+vs\.?\s+[^()]+$",
+		@"^[\w\.""'-]+:(\s*.+\|)?\s*[^()]+\s+vs\.?\s+\([^\)]+\)$",
+		@"^[\w\.""'-]+:(\s*.+\|)?\s*[^()]+\s+vs\.?\s+[^()]+$",
+		@"^""[a-zA-Z0-9\s]+: \([a-zA-Z0-9""]+\) vs \([a-zA-Z0-9""]+\)""$"
 	};
 	public static bool IsNameValid(string name) => patterns.Any(pattern => Regex.IsMatch(name, pattern, RegexOptions.IgnoreCase));
 }
--- APITests/Osu/LobbyNameCheckerTests.cs
+++ APITests/Osu/LobbyNameCheckerTests.cs
@@ -10,7 +10,6 @@ public class LobbyNameCheckerTests
 	[TestCase("PDT: (Пиздатые Плюшки) vs (Взрывные @everyone котюнчики)")]
 	[TestCase("CFC: (mp close) vs (フフフ)")]
 	[TestCase("BotB: (--... ..--- --...) vs (:monkey: but emoji)")]
-	[TestCase("CC : (8mi8) vs (-- Waffle --)")]
 	[TestCase("CAT18: 1k-9k L12 |(Minami-Kotori) vs (KomachiBaka)")]
 	[TestCase("\"6 digit cup: (group\"\"3\"\") vs (group\"\"3\"\")\"")]
 	[TestCase("AHSESPORTS: (The S.S.) vs (over salted utakus)")]
@@ -18,11 +17,18 @@ public class LobbyNameCheckerTests
 	[TestCase("BGCC2: Водоземската Мафия vs. po4ina")]
 	[TestCase("BGCC2: Водоземската Мафия VS. po4ina")]
 	[TestCase("BGCC2: Водоземската Мафия VS po4ina")]
+	[TestCase("CC: (8mi8) vs (-- Waffle --)")]
+	[TestCase("CC: (()()()) vs ((((((((()))")]
+	[TestCase("CC: ()) vs. (()")]
+	[TestCase("CC: (() vs ())")]
 	public void Patterns_Regex_ShouldMatch(string name) => Assert.That(LobbyNameChecker.IsNameValid(name), Is.True);
 
 	[Test]
 	[TestCase("test (a) vs. (b)")]
 	[TestCase("test (a) vs (b)")]
 	[TestCase("WDTWE (:smirk_cat:) vs (RAMSING)")]
+	[TestCase("WDTWE :smirk_cat:) vs (RAMSING")]
+	[TestCase("WDTWE :smirk_cat: vs RAMSING")]
+	[TestCase("CC : (8mi8) vs (-- Waffle --)")]
 	public void Patterns_Regex_ShouldNotMatch(string name) => Assert.That(LobbyNameChecker.IsNameValid(name), Is.False);
 }
