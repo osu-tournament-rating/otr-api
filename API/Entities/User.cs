baaa2bc4595d80e47e5cdbbc0c9d6984a20382e2
hburn7 <hburnett777@gmail.com>
2023-08-29 22:13:34 -0400

    Added TODO about how to impl user in the future

--- API/Entities/User.cs
+++ API/Entities/User.cs
@@ -10,4 +10,6 @@ public class User : EntityBase
 	public int PlayerId { get; set; }
 	[Column("last_login")]
 	public DateTime LastLogin { get; set; }
+	
+	// TODO: Add information about user roles (Admin, etc.) -- These could also be displayed in the frontend as a badge
 }
