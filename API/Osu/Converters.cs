d1c6ab3d5b5d1b707e5907d202dfffe25e1154fb
hburn7 <hburnett777@gmail.com>
2023-08-31 15:07:57 -0400

    Deleted broken converter code

--- API/Osu/Converters.cs
+++ API/Osu/Converters.cs
@@ -15,24 +15,7 @@ public class Converters
 			// TODO: Needs unit test
 			foreach (var score in game.Scores)
 			{
-				var matchData = new PlayerMatchData
-				{
-					OsuMatchId = long.TryParse(data.Match.MatchId, out long osuMatchId) ? osuMatchId : 0,
-					GameId = long.TryParse(game.GameId, out long gameId) ? gameId : 0,
-					ScoringType = game.ScoringType,
-					OsuBeatmapId = long.TryParse(game.BeatmapId, out long beatmapId) ? beatmapId : 0,
-					GameRawMods = int.TryParse(game.Mods, out int gameRawMods) ? gameRawMods : 0,
-					RawMods = int.TryParse(score.EnabledMods, out int rawMods) ? rawMods : 0,
-					MatchName = data.Match.Name,
-					Mode = game.PlayMode,
-					MatchStartDate = data.Match.StartTime,
-					TeamType = game.TeamType,
-					Team = score.Team,
-					Score = double.TryParse(score.PlayerScore, out double playerScore) ? playerScore : 0,
-					PlayerId = int.TryParse(score.UserId, out int playerId) ? playerId : 0
-				};
-
-				matchDatas.Add(matchData);
+				// todo: convert
 			}
 		}
 
