c09a76b3565f1ecc97a29885421923c7acec293d
hburn7 <hburnett777@gmail.com>
2023-09-02 21:54:24 -0400

    cleanup, wip joins for relations

--- API/Services/Implementations/PlayerService.cs
+++ API/Services/Implementations/PlayerService.cs
@@ -8,9 +8,7 @@ namespace API.Services.Implementations;
 
 public class PlayerService : ServiceBase<Player>, IPlayerService
 {
-	public PlayerService(ICredentials credentials, ILogger<PlayerService> logger) : base(credentials, logger)
-	{
-	}
+	public PlayerService(ICredentials credentials, ILogger<PlayerService> logger) : base(credentials, logger) {}
 
 	public async Task<Player?> GetByOsuIdAsync(long osuId)
 	{
@@ -19,7 +17,7 @@ public class PlayerService : ServiceBase<Player>, IPlayerService
 			return await connection.QuerySingleOrDefaultAsync<Player>("SELECT * FROM players WHERE osu_id = @OsuId", new { OsuId = osuId });
 		}
 	}
-	
+
 	public async Task<IEnumerable<Player>> GetByOsuIdsAsync(IEnumerable<long> osuIds)
 	{
 		using (var connection = new NpgsqlConnection(ConnectionString))
@@ -27,6 +25,61 @@ public class PlayerService : ServiceBase<Player>, IPlayerService
 			return await connection.QueryAsync<Player>("SELECT * FROM players WHERE osu_id = ANY(@OsuIds)", new { OsuIds = osuIds });
 		}
 	}
+
+	// public async Task<IEnumerable<Player>> GetByOsuIdsAsync(IEnumerable<long> osuIds)
+	// {
+	// 	using (var connection = new NpgsqlConnection(ConnectionString))
+	// 	{
+	// 		const string sql = @"SELECT * FROM players p
+ //        INNER JOIN ratings r on p.id = r.player_id
+ //        INNER JOIN ratinghistories h on p.id = h.player_id
+ //        INNER JOIN match_scores ms on p.id = ms.player_id
+ //        INNER JOIN games g on ms.game_id = g.id
+ //        INNER JOIN matches m on g.match_id = m.id WHERE osu_id = ANY(@OsuIds)";
+ //
+	// 		var playerDictionary = new Dictionary<long, Player>();
+	// 		var ratingIds = new HashSet<(int, string)>();  // PlayerId, Mode
+	// 		var matchScoreIds = new HashSet<(int, int)>(); // GameId, PlayerId
+	// 		var matchDictionary = new Dictionary<int, Match>();
+ //
+	// 		await connection.QueryAsync<Player, Rating, RatingHistory, MatchScore, Game, Match, Player>(sql, (player, rating, ratingHistory, matchScore,
+	// 			game, match) =>
+	// 		{
+	// 			if (!playerDictionary.TryGetValue(player.Id, out var currentPlayer))
+	// 			{
+	// 				currentPlayer = player;
+	// 				currentPlayer.Ratings = new List<Rating>();
+	// 				currentPlayer.RatingHistories = new List<RatingHistory>();
+	// 				currentPlayer.Matches = new List<Match>();
+	// 				playerDictionary.Add(currentPlayer.Id, currentPlayer);
+	// 			}
+ //
+	// 			if (ratingIds.Add((rating.PlayerId, rating.Mode)))
+	// 			{
+	// 				currentPlayer.Ratings.Add(rating);
+	// 			}
+ //
+	// 			if (matchScoreIds.Add((matchScore.GameId, matchScore.PlayerId)))
+	// 			{
+	// 				currentPlayer.RatingHistories.Add(ratingHistory);
+	// 			}
+ //
+	// 			if (!matchDictionary.TryGetValue(match.Id, out var currentMatch))
+	// 			{
+	// 				currentMatch = match;
+	// 				currentMatch.Games = new List<Game>();
+	// 				matchDictionary.Add(currentMatch.Id, currentMatch);
+	// 				currentPlayer.Matches.Add(currentMatch);
+	// 			}
+ //
+	// 			currentMatch.Games.Add(game);
+ //
+	// 			return currentPlayer;
+	// 		}, new { OsuIds = osuIds }, splitOn: "id, id, id, id, id");
+ //
+	// 		return playerDictionary.Values;
+	// 	}
+	// }
 
 	public async Task<int> GetIdByOsuIdAsync(long osuId)
 	{
