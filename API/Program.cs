b75610ec9b0fd610f532b2f0d159fc0d9544d5e8
hburn7 <hburnett777@gmail.com>
2023-09-09 15:18:04 -0400

    import reference for hosted service

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using API;
 using API.Configurations;
 using API.DTOs;
 using API.Models;
+using API.Osu;
 using API.Osu.Multiplayer;
 using API.Services.Implementations;
 using API.Services.Interfaces;
