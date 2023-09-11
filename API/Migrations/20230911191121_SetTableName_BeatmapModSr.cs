ca4e6bb25ba67b60c533fed2974ac0fbd6ac2d07
hburn7 <hburnett777@gmail.com>
2023-09-11 17:07:36 -0400

    Add migrations

---
+++ API/Migrations/20230911191121_SetTableName_BeatmapModSr.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetTableName_BeatmapModSr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "BeatmapModSr",
+                newName: "beatmap_mod_sr");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "beatmap_mod_sr",
+                newName: "BeatmapModSr");
+        }
+    }
+}
