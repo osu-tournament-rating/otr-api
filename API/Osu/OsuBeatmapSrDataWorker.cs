3414bb4d42ef42b4438ecda3d838ca3cc53f2a92
hburn7 <hburnett777@gmail.com>
2023-09-12 20:34:26 -0400

    Revert parallel processing of beatmap srs

--- API/Osu/OsuBeatmapSrDataWorker.cs
+++ API/Osu/OsuBeatmapSrDataWorker.cs
@@ -40,50 +40,25 @@ public class OsuBeatmapSrDataWorker : BackgroundService
 	private async Task ProcessBeatmapModSrsAsync(IBeatmapService beatmapService)
 	{
 		var maps = await beatmapService.GetUnprocessedSrBeatmapIdsAsync();
-
-		var semaphore = new SemaphoreSlim(4);
-    
-		var batchSize = 100;
-		for (int i = 0; i < maps.Count; i += batchSize)
+		foreach ((int mapId, OsuEnums.Mods mods) in maps)
 		{
-			var currentBatch = maps.Skip(i).Take(batchSize).ToList();
-			var tasks = currentBatch.Select(map => ProcessMapAsync(map, semaphore, beatmapService)).ToList();
-
-			// Wait for the current batch to complete before processing the next one.
-			await Task.WhenAll(tasks);
-		}
-	}
-
-	private async Task ProcessMapAsync((int mapId, OsuEnums.Mods mods) map, SemaphoreSlim semaphore, IBeatmapService beatmapService)
-	{
-		await semaphore.WaitAsync(); // Acquire semaphore
-
-		try
-		{
-			int mapId = map.mapId;
-			OsuEnums.Mods mods = map.mods;
 			long osuMapId = await beatmapService.GetBeatmapIdAsync(mapId);
 			var beatmap = await _osuApiService.GetBeatmapAsync(osuMapId, mods);
-
+			
 			if (beatmap == null)
 			{
 				_logger.LogWarning("Failed to fetch beatmap while processing mod sr {BeatmapId} (result from API was null)", osuMapId);
-				return;
+				continue;
 			}
-
+			
 			await beatmapService.InsertModSrAsync(new BeatmapModSr
 			{
 				BeatmapId = mapId,
 				Mods = (int)mods,
 				PostModSr = beatmap.Sr
 			});
-
+			
 			_logger.LogDebug("Inserted mod sr for {BeatmapId} with mods {Mods}", osuMapId, mods);
 		}
-		finally
-		{
-			semaphore.Release(); // Release semaphore
-		}
 	}
-
 }
