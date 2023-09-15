f6b240c3f263817ed26e5b88a42b473c95bdd57f
hburn7 <hburnett777@gmail.com>
2023-09-15 12:21:34 -0400

    Migrations

---
+++ API/Migrations/20230915145743_Match_UserSubmissions.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Match_UserSubmissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "submitted_by",
+                table: "matches");
+
+            migrationBuilder.AddColumn<int>(
+                name: "submitted_by_user",
+                table: "matches",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_matches_submitted_by_user",
+                table: "matches",
+                column: "submitted_by_user");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_matches_users_submitted_by_user",
+                table: "matches",
+                column: "submitted_by_user",
+                principalTable: "users",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_matches_users_submitted_by_user",
+                table: "matches");
+
+            migrationBuilder.DropIndex(
+                name: "IX_matches_submitted_by_user",
+                table: "matches");
+
+            migrationBuilder.DropColumn(
+                name: "submitted_by_user",
+                table: "matches");
+
+            migrationBuilder.AddColumn<int>(
+                name: "submitted_by",
+                table: "matches",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
