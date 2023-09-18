e580c0d0351ceb48b35794b02b02397047aa1583
hburn7 <hburnett777@gmail.com>
2023-09-18 19:21:25 -0400

    Added some docs for stats

--- API/DTOs/Unmapped_PlayerStatisticsDTO.cs
+++ API/DTOs/Unmapped_PlayerStatisticsDTO.cs
@@ -3,19 +3,26 @@ namespace API.DTOs;
 public class Unmapped_PlayerStatisticsDTO
 {
 	/// <summary>
-	/// e.g. Platinum, Diamond
+	/// The current ranking of the user, e.g. Platinum, Diamond
 	/// </summary>
 	public string Ranking { get; set; } = null!;
+	/// <summary>
+	/// The next ranking the user can achieve, e.g. Diamond, Master
+	/// </summary>
 	public string? NextRanking { get; set; }
+	/// <summary>
+	/// The current rating of the user for the selected mode
+	/// </summary>
 	public int Rating { get; set; }
+	/// <summary>
+	/// Global o!TR rank of the user for the selected mode
+	/// </summary>
 	public int GlobalRank { get; set; }
 	public int CountryRank { get; set; }
 	public double Percentile { get; set; }
 	public int RatingDelta { get; set; }
 	public int RatingForNextRank { get; set; }
 	public int HighestRating { get; set; }
-	public int HighestGlobalRank { get; set; }
-	public double HighestPercentile { get; set; }
 	public int MatchesPlayed { get; set; }
 	public int GamesPlayed { get; set; }
 	public int MatchesWon { get; set; }
@@ -43,6 +50,11 @@ public class Unmapped_PlayerStatisticsDTO
 	public string? MostPlayedOpponent { get; set; }
 	public string? BestPerformingOpponent { get; set; }
 	public string? WorstPerformingOpponent { get; set; }
+	/// <summary>
+	/// On the frontend, say the user selects to view a 30 day chart.
+	/// We show the rating gained or lost since the start of that period (30
+	/// days ago to now)
+	/// </summary>
 	public int RatingGainedSincePeriod { get; set; }
 	public bool IsRatingPositiveTrend { get; set; }
 }
