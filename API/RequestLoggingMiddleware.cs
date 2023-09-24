509cca69220792e220de60b3f5c3e4e27baf9a7e
hburn7 <hburnett777@gmail.com>
2023-09-24 19:06:22 -0400

    Truncate verbose logs

--- API/RequestLoggingMiddleware.cs
+++ API/RequestLoggingMiddleware.cs
@@ -64,6 +64,11 @@ public class RequestLoggingMiddleware
 		//We need to reset the reader for the response so that the client can read it.
 		response.Body.Seek(0, SeekOrigin.Begin);
 
+		if (text.Length > 100 && response.StatusCode == 200)
+		{
+			text = text[..100] + "...";
+		}
+
 		//Return the string for the response, including the status code (e.g. 200, 404, 401, etc.)
 		_logger.LogInformation("Returned status {StatusCode} to {Scheme} {Method} {Host}{Path}{QueryString} with body '{Body}'", response.StatusCode, 
 			response.HttpContext.Request.Scheme, response.HttpContext.Request.Method, response.HttpContext.Request.Host, response.HttpContext.Request.Path, response.HttpContext.Request.QueryString, text);
