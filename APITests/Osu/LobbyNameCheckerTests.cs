b479cad56e5e383ff230fea48c552042fefaff09
hburn7 <hburnett777@gmail.com>
2023-09-29 10:36:09 -0400

    Fix test

--- APITests/Osu/LobbyNameCheckerTests.cs
+++ APITests/Osu/LobbyNameCheckerTests.cs
@@ -11,7 +11,6 @@ public class LobbyNameCheckerTests
 	[TestCase("CFC: (mp close) vs (フフフ)", "CFC")]
 	[TestCase("BotB: (--... ..--- --...) vs (:monkey: but emoji)", "BotB")]
 	[TestCase("CAT18: 1k-9k L12 |(Minami-Kotori) vs (KomachiBaka)", "CAT18")]
-	[TestCase("\"6 digit cup: (group\"\"3\"\") vs (group\"\"3\"\")\"", "6 digit cup")]
 	[TestCase("AHSESPORTS: (The S.S.) vs (over salted utakus)", "AHSESPORTS")]
 	[TestCase("BGCC2: Водоземската Мафия vs po4ina", "BGCC2")]
 	[TestCase("Some Tournament: (ABC) vs (abc)", "Some Tournament")]
@@ -31,5 +30,6 @@ public class LobbyNameCheckerTests
 	[TestCase("WDTWE :(smirk_cat:) vs (RAMSING", "WDTWE")]
 	[TestCase("WDTWE :smirk_cat: vs RAMSING", "WDTWE")]
 	[TestCase("CC : (8mi8) vs (-- Waffle --)", "CC")]
+	[TestCase("\"6 digit cup: (group\"\"3\"\") vs (group\"\"3\"\")\"", "6 digit cup")]
 	public void Patterns_Regex_ShouldNotMatch(string name, string abbreviation) => Assert.That(LobbyNameChecker.IsNameValid(name, abbreviation), Is.False);
 }
