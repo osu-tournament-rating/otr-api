516beba77367b2815b64371277dd7bb5f9fdab7e
hburn7 <hburnett777@gmail.com>
2023-09-30 09:20:17 -0400

    Update lobby name checker

--- APITests/Osu/LobbyNameCheckerTests.cs
+++ APITests/Osu/LobbyNameCheckerTests.cs
@@ -11,25 +11,31 @@ public class LobbyNameCheckerTests
 	[TestCase("CFC: (mp close) vs (フフフ)", "CFC")]
 	[TestCase("BotB: (--... ..--- --...) vs (:monkey: but emoji)", "BotB")]
 	[TestCase("CAT18: 1k-9k L12 |(Minami-Kotori) vs (KomachiBaka)", "CAT18")]
-	[TestCase("\"6 digit cup: (group\"\"3\"\") vs (group\"\"3\"\")\"", "6 digit cup")]
 	[TestCase("AHSESPORTS: (The S.S.) vs (over salted utakus)", "AHSESPORTS")]
-	[TestCase("BGCC2: Водоземската Мафия vs po4ina", "BGCC2")]
 	[TestCase("Some Tournament: (ABC) vs (abc)", "Some Tournament")]
-	[TestCase("BGCC2: Водоземската Мафия vs. po4ina", "BGCC2")]
-	[TestCase("BGCC2: Водоземската Мафия VS. po4ina", "BGCC2")]
-	[TestCase("BGCC2: Водоземската Мафия VS po4ina", "BGCC2")]
 	[TestCase("CC: (8mi8) vs (-- Waffle --)", "CC")]
 	[TestCase("CC: (()()()) vs ((((((((()))", "CC")]
 	[TestCase("CC: ()) vs. (()", "CC")]
 	[TestCase("CC: (() vs ())", "CC")]
-	public void Patterns_Regex_ShouldMatch(string name, string abbreviation) => Assert.That(LobbyNameChecker.IsNameValid(name, abbreviation), Is.True);
-
-	[Test]
 	[TestCase("test (a) vs. (b)", "test")]
 	[TestCase("test (a) vs (b)", "test")]
 	[TestCase("WDTWE (:smirk_cat:) vs (RAMSING)", "WDTWE")]
-	[TestCase("WDTWE :(smirk_cat:) vs (RAMSING", "WDTWE")]
-	[TestCase("WDTWE :smirk_cat: vs RAMSING", "WDTWE")]
 	[TestCase("CC : (8mi8) vs (-- Waffle --)", "CC")]
+	[TestCase("6 digit cup: (group\"\"3\"\") vs (group\"\"3\"\")\"", "6 digit cup")]
+	public void Patterns_Regex_ShouldMatch(string name, string abbreviation) => Assert.That(LobbyNameChecker.IsNameValid(name, abbreviation), Is.True);
+
+	[Test]
+	[TestCase("THIS SHOULD FAIL", "THIS SHOULD FAIL")] // Tests for teams existing (e.g. "(a) vs (b)")
+	[TestCase("THIS: SHOULD FAIL", "THIS")]
+	[TestCase("(a) vs. (b)", "SomeAbbreviationThat'sMissing")]
+	[TestCase("Test: A vs. (b)", "Test")]
+	[TestCase("Test: (A) vs. b", "Test")]
+	[TestCase("BGCC2: Водоземската Мафия vs. po4ina", "BGCC2")]
+	[TestCase("BGCC2: Водоземската Мафия VS. po4ina", "BGCC2")]
+	[TestCase("BGCC2: Водоземската Мафия VS po4ina", "BGCC2")]
+	[TestCase("\"6 digit cup: (group\"\"3\"\") vs (group\"\"3\"\")\"", "6 digit cup")]
+	[TestCase("BGCC2: Водоземската Мафия vs po4ina", "BGCC2")]
+	[TestCase("WDTWE :smirk_cat: vs RAMSING", "WDTWE")]
+	[TestCase("WDTWE :(smirk_cat:) vs (RAMSING", "WDTWE")]
 	public void Patterns_Regex_ShouldNotMatch(string name, string abbreviation) => Assert.That(LobbyNameChecker.IsNameValid(name, abbreviation), Is.False);
 }
