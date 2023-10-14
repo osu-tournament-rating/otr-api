bf6d210bfdda9b5b9778715acb95e75ff5d3d43a
hburn7 <hburnett777@gmail.com>
2023-10-14 16:43:22 -0400

    migrations

---
+++ API/Migrations/20231014175537_Tournament_Unique_Name_Abbreviation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tournament_Unique_Name_Abbreviation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_tournaments_name_abbreviation",
+                table: "tournaments",
+                columns: new[] { "name", "abbreviation" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_tournaments_name_abbreviation",
+                table: "tournaments");
+        }
+    }
+}
