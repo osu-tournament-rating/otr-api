21723292c45271e3e2f52526b6936503829dc63f
hburn7 <hburnett777@gmail.com>
2023-10-22 15:35:08 -0400

    Logging memory efficiencies

--- API/RequestLoggingMiddleware.cs
+++ API/RequestLoggingMiddleware.cs
@@ -45,30 +45,31 @@ public class RequestLoggingMiddleware
 	private async Task LogRequest(HttpRequest request)
 	{
 		request.EnableBuffering();
-    
+
 		var body = request.Body;
 		try
 		{
 			// Create a new memory stream.
 			var memStream = new MemoryStream();
 			await body.CopyToAsync(memStream);
-        
+
 			// Reset the position of the memory stream to be read from the beginning.
 			memStream.Position = 0;
-        
+
 			// Read from the memory stream and convert to string for logging.
 			var buffer = new byte[memStream.Length];
 			await memStream.ReadAsync(buffer, 0, buffer.Length);
 			var bodyAsText = Encoding.UTF8.GetString(buffer);
-        
+
 			if (bodyAsText.Length > 500)
 			{
 				bodyAsText = bodyAsText[..500] + "...";
 			}
-        
+
 			_logger.LogInformation("User with claims {@Claims} on scheme {Scheme} requests {Method} {Host}{Path}{QueryString} with body '{Body}'",
-				request.HttpContext.User.Claims, request.Scheme, request.Method, request.Host, request.Path, request.QueryString, bodyAsText);
-        
+				request.HttpContext.User.Claims, request.Scheme, request.Method, request.Host, request.Path,
+				request.QueryString, bodyAsText);
+
 			// Reset the memory stream position again before assigning it back to the request body to be read by subsequent middlewares.
 			memStream.Position = 0;
 			request.Body = memStream;
@@ -82,25 +83,28 @@ public class RequestLoggingMiddleware
 			}
 		}
 	}
-	
+
 	private async Task LogResponse(HttpResponse response)
 	{
 		//We need to read the response stream from the beginning...
 		response.Body.Seek(0, SeekOrigin.Begin);
 
 		//...and copy it into a string
-		string text = await new StreamReader(response.Body).ReadToEndAsync();
+		var buff = new char[100];
+		await new StreamReader(response.Body).ReadAsync(buff, 0, (int)Math.Min(100, response.Body.Length));
 
 		//We need to reset the reader for the response so that the client can read it.
 		response.Body.Seek(0, SeekOrigin.Begin);
-
-		if (text.Length > 100 && response.StatusCode == 200)
+		
+		string text = new(buff);
+		if (text.Length == 100)
 		{
-			text = text[..100] + "...";
+			text += "...";
 		}
 
 		//Return the string for the response, including the status code (e.g. 200, 404, 401, etc.)
-		_logger.LogInformation("Returned status {StatusCode} to {Scheme} {Method} {Host}{Path}{QueryString} with body '{Body}'", response.StatusCode, 
-			response.HttpContext.Request.Scheme, response.HttpContext.Request.Method, response.HttpContext.Request.Host, response.HttpContext.Request.Path, response.HttpContext.Request.QueryString, text);
+		_logger.LogInformation("Returned status {StatusCode} to {Scheme} {Method} {Host}{Path}{QueryString} with body '{Body}'", response.StatusCode,
+			response.HttpContext.Request.Scheme, response.HttpContext.Request.Method, response.HttpContext.Request.Host, response.HttpContext.Request.Path,
+			response.HttpContext.Request.QueryString, text);
 	}
 }
