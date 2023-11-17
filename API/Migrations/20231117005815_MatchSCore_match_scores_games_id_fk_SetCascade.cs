f0f251104774fd3806b27b2797ec62f00c4ca2a9
hburn7 <hburnett777@gmail.com>
2023-11-16 19:58:39 -0500

    Cascade migrations complete

---
+++ API/Migrations/20231117005815_MatchSCore_match_scores_games_id_fk_SetCascade.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class MatchSCore_match_scores_games_id_fk_SetCascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "match_scores_games_id_fk",
+                table: "match_scores");
+
+            migrationBuilder.AddForeignKey(
+                name: "match_scores_games_id_fk",
+                table: "match_scores",
+                column: "game_id",
+                principalTable: "games",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "match_scores_games_id_fk",
+                table: "match_scores");
+
+            migrationBuilder.AddForeignKey(
+                name: "match_scores_games_id_fk",
+                table: "match_scores",
+                column: "game_id",
+                principalTable: "games",
+                principalColumn: "id");
+        }
+    }
+}
