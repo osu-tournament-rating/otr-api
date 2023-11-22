233b8534fa725cdd244408595ab614e1f35d088b
hburn7 <hburnett777@gmail.com>
2023-11-22 00:06:24 -0500

    Tier updates

--- API/Utilities/RatingUtils.cs
+++ API/Utilities/RatingUtils.cs
@@ -2,32 +2,37 @@ namespace API.Utilities;
 
 public static class RatingUtils
 {
-	public const int RatingBronze = 499;
-	public const int RatingSilver = 899;
-	public const int RatingGold = 1299;
-	public const int RatingPlatinum = 1599;
-	public const int RatingDiamond = 1899;
-	public const int RatingMaster = 2199;
-	public const int RatingGrandmaster = 2499;
-	public const int RatingEliteGrandmaster = 2799;
+	// Players at or below the threshold are considered to be in the previous tier
 	
-	public static string GetTier(int rating) => rating switch
+	// Bronze: 100-499
+	public const int RatingSilver = 500;
+	public const int RatingGold = 800;
+	public const int RatingPlatinum = 1100;
+	public const int RatingRuby = 1400;
+	public const int RatingDiamond = 1700;
+	public const int RatingMaster = 2000;
+	public const int RatingGrandmaster = 2300;
+	public const int RatingEliteGrandmaster = 2600;
+	
+	public static string GetTier(double rating) => rating switch
 	{
 		< RatingSilver => "Bronze",
 		< RatingGold => "Silver",
 		< RatingPlatinum => "Gold",
-		< RatingDiamond => "Platinum",
+		< RatingRuby => "Platinum",
+		< RatingDiamond => "Ruby",
 		< RatingMaster => "Diamond",
 		< RatingGrandmaster => "Master",
 		< RatingEliteGrandmaster => "Grandmaster",
 		_ => "Elite Grandmaster"
 	};
 	
-	public static string GetNextTier(int rating) => rating switch
+	public static string GetNextTier(double rating) => rating switch
 	{
 		< RatingSilver => GetTier(RatingSilver),
 		< RatingGold => GetTier(RatingGold),
 		< RatingPlatinum => GetTier(RatingPlatinum),
+		< RatingRuby => GetTier(RatingRuby),
 		< RatingDiamond => GetTier(RatingDiamond),
 		< RatingMaster => GetTier(RatingMaster),
 		< RatingGrandmaster => GetTier(RatingGrandmaster),
@@ -35,11 +40,12 @@ public static class RatingUtils
 		_ => GetTier(RatingEliteGrandmaster)
 	};
 
-	public static int GetRatingForNextTier(int rating) => rating switch
+	public static double GetRatingForNextTier(double rating) => rating switch
 	{
 		< RatingSilver => RatingSilver - rating,
 		< RatingGold => RatingGold - rating,
 		< RatingPlatinum => RatingPlatinum - rating,
+		< RatingRuby => RatingRuby - rating,
 		< RatingDiamond => RatingDiamond - rating,
 		< RatingMaster => RatingMaster - rating,
 		< RatingGrandmaster => RatingGrandmaster - rating,
@@ -54,12 +60,13 @@ public static class RatingUtils
 	/// </summary>
 	/// <param name="rating"></param>
 	/// <returns></returns>
-	public static int GetRatingDelta(int rating) => rating switch
+	public static double GetRatingDelta(double rating) => rating switch
 	{
-		< RatingSilver => RatingSilver - RatingBronze,
+		< RatingSilver => RatingSilver/* - RatingBronze*/,
 		< RatingGold => RatingGold - RatingSilver,
 		< RatingPlatinum => RatingPlatinum - RatingGold,
-		< RatingDiamond => RatingDiamond - RatingPlatinum,
+		< RatingRuby => RatingRuby - RatingPlatinum,
+		< RatingDiamond => RatingDiamond - RatingRuby,
 		< RatingMaster => RatingMaster - RatingDiamond,
 		< RatingGrandmaster => RatingGrandmaster - RatingMaster,
 		< RatingEliteGrandmaster => RatingEliteGrandmaster - RatingGrandmaster,
