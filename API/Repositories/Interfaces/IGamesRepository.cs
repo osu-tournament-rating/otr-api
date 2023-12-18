5fc468b97c791c642bb6014da36e49c22031d5fd
hburn7 <hburnett777@gmail.com>
2023-12-18 18:32:34 -0500

    Remove unused code in games repository

--- API/Repositories/Interfaces/IGamesRepository.cs
+++ API/Repositories/Interfaces/IGamesRepository.cs
@@ -4,7 +4,4 @@ namespace API.Repositories.Interfaces;
 
 public interface IGamesRepository : IRepository<Game>
 {
-	Task<int> CountGameWinsAsync(long osuPlayerId, int mode, DateTime fromTime);
-	Task<string?> MostPlayedTeammateNameAsync(long osuPlayerId, int mode, DateTime fromDate);
-	Task<string?> MostPlayedOpponentNameAsync(long osuPlayerId, int mode, DateTime fromDate);
 }
