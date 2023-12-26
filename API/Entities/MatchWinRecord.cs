18942545dee0a33c87b6a7e9dc44f32cbc64540a
hburn7 <hburnett777@gmail.com>
2023-12-26 12:54:03 -0500

    Add MatchType field & migrations

--- API/Entities/MatchWinRecord.cs
+++ API/Entities/MatchWinRecord.cs
@@ -10,20 +10,37 @@ public class MatchWinRecord
 {
 	[Column("id")]
 	public int Id { get; set; }
+
 	[Column("match_id")]
 	public int MatchId { get; set; }
+
+	// Team arrays can represent individual players in individual matches (head to head)
 	[Column("team1")]
 	public int[] Team1 { get; set; } = Array.Empty<int>();
+
 	[Column("team2")]
 	public int[] Team2 { get; set; } = Array.Empty<int>();
+
 	[Column("team1_points")]
 	public int Team1Points { get; set; }
+
 	[Column("team2_points")]
 	public int Team2Points { get; set; }
+
 	[Column("winner_team")]
 	public int? WinnerTeam { get; set; }
+
 	[Column("loser_team")]
 	public int? LoserTeam { get; set; }
+	[Column("match_type")]
+	public MatchType MatchType { get; set; }
+
 	[InverseProperty("WinRecord")]
 	public virtual Match Match { get; set; }
+}
+
+public enum MatchType
+{
+	Team = 1,
+	Individual = 2
 }
