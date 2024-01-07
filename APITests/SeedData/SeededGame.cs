21503f5dc22f7c8e868e161f0c0bf9c6164617f3
hburn7 <hburnett777@gmail.com>
2024-01-06 21:39:49 -0500

    Modify seed data

--- APITests/SeedData/SeededGame.cs
+++ APITests/SeedData/SeededGame.cs
@@ -4,22 +4,68 @@ namespace APITests.SeedData;
 
 public static class SeededGame
 {
-	public static Game Get() => new()
+	private static readonly Random _rand = new();
+
+	public static ICollection<Game> Generate(int matchId, int amount)
 	{
-		Id = 198743,
-		MatchId = 35214,
-		BeatmapId = 24245,
-		PlayMode = 0,
-		ScoringType = 3,
-		TeamType = 0,
-		Mods = 0,
-		PostModSr = 6.36389,
-		GameId = 502333236,
-		VerificationStatus = 2,
-		RejectionReason = 0,
-		Created = new DateTime(2023,09,14),
-		StartTime = new DateTime(2023,03,10),
-		EndTime = new DateTime(2023,03,10),
-		Updated = new DateTime(2023,11,04),
-	};
+		var ls = new List<Game>();
+
+		for (int i = 0; i < amount; i++)
+		{
+			ls.Add(Generate(matchId));
+		}
+
+		return ls;
+	}
+
+	private static Game Generate(int matchId)
+	{
+		var game = new Game
+		{
+			Id = _rand.Next(),
+			MatchId = matchId,
+			BeatmapId = 24245,
+			PlayMode = 0,
+			ScoringType = 3,
+			TeamType = 0,
+			Mods = 0,
+			PostModSr = 6.36389,
+			GameId = 502333236,
+			VerificationStatus = 2,
+			RejectionReason = 0,
+			Created = new DateTime(2023, 09, 14),
+			StartTime = new DateTime(2023, 03, 10),
+			EndTime = new DateTime(2023, 03, 10),
+			Updated = new DateTime(2023, 11, 04),
+			Beatmap = new Beatmap
+			{
+				Id = _rand.Next(),
+				Artist = "Bob Jilly Jones",
+				BeatmapId = _rand.Next(),
+				Bpm = 170.0,
+				MapperId = _rand.NextInt64(),
+				MapperName = "Smithy Lilly",
+				Sr = 5.3,
+				AimDiff = 3.2,
+				SpeedDiff = 5.3 - 3.2,
+				Cs = 4.0,
+				Ar = 9.3,
+				Hp = 5.0,
+				Od = 8.0,
+				DrainTime = 174,
+				Length = 160,
+				Title = "SmithyLillyBobbyJonesy",
+				DiffName = "Smithy",
+				GameMode = 0,
+				CircleCount = 50,
+				SliderCount = 5,
+				SpinnerCount = 1,
+				MaxCombo = 57,
+				Created = new DateTime(2020, 1, 1)
+			}
+		};
+
+		game.MatchScores = SeededMatchScore.GetScoresForGame(game.Id, 4, 4, 0).ToList();
+		return game;
+	}
 }
--- APITests/SeedData/SeededMatchScore.cs
+++ APITests/SeedData/SeededMatchScore.cs
@@ -4,23 +4,66 @@ namespace APITests.SeedData;
 
 public static class SeededMatchScore
 {
-	public static MatchScore Get() => new()
+	private static readonly Random _rand = new();
+	private const int MAX_COMBO_LIMIT = 1200;
+	private const int COUNT_50_LIMIT = 3;
+	private const int COUNT_100_LIMIT = 50;
+	private const int COUNT_300_LIMIT = (COUNT_50_LIMIT + COUNT_100_LIMIT + MAX_COMBO_LIMIT) * 2;
+	private const int MISS_LIMIT = 10;
+
+	public static IEnumerable<MatchScore> GetScoresForGame(int gameId, int? amountBlue = null, int? amountRed = null, int? amountHeadToHead = null)
 	{
-		Id = 749311,
-		GameId = 274393,
-		Team = 1,
-		Score = 700810,
-		MaxCombo = 426,
-		Count50 = 0,
-		Count100 = 21,
-		Count300 = 464,
-		CountMiss = 8,
-		Perfect = false,
-		Pass = true,
-		EnabledMods = 0,
-		CountKatu = 8,
-		CountGeki = 114,
-		PlayerId = 4962,
-		IsValid = true,
-	};
+		var ret = new List<MatchScore>();
+
+		if (amountBlue.HasValue)
+		{
+			for (int i = 0; i < amountBlue; i++)
+			{
+				ret.Add(Generate(gameId, 1));
+			}
+		}
+
+		if (amountRed != null)
+		{
+			for (int i = 0; i < amountBlue; i++)
+			{
+				ret.Add(Generate(gameId, 2));
+			}
+		}
+
+		if (amountHeadToHead != null)
+		{
+			for (int i = 0; i < amountBlue; i++)
+			{
+				ret.Add(Generate(gameId, 0));
+			}
+		}
+
+		return ret;
+	}
+
+	private static MatchScore Generate(int gameId, int team)
+	{
+		int misses = _rand.Next() % MISS_LIMIT;
+		bool perfect = misses == 0;
+
+		return new MatchScore
+		{
+			Id = _rand.Next(),
+			GameId = gameId,
+			Team = team,
+			Score = _rand.NextInt64(),
+			MaxCombo = _rand.Next() % 2000,
+			Count50 = _rand.Next() % COUNT_50_LIMIT,
+			Count100 = _rand.Next() % COUNT_100_LIMIT,
+			Count300 = _rand.Next() % COUNT_300_LIMIT,
+			CountKatu = _rand.Next() % (COUNT_100_LIMIT / 7),
+			CountGeki = _rand.Next() % (COUNT_300_LIMIT / 7),
+			CountMiss = misses,
+			Perfect = perfect,
+			PlayerId = _rand.Next() % 10000,
+			IsValid = true,
+			EnabledMods = null
+		};
+	}
 }
