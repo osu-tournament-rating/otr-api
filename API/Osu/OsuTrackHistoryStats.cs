445f32fa1cb9cdb479847555d37bb17f32c46696
hburn7 <hburnett777@gmail.com>
2024-01-29 20:19:58 -0500

    Move OsuTrackHistoryStats to new file

---
+++ API/Osu/OsuTrackHistoryStats.cs
@@ -0,0 +1,11 @@
+using Newtonsoft.Json;
+
+namespace API.Osu;
+
+public class OsuTrackHistoryStats
+{
+	[JsonProperty("pp_rank")]
+	public int Rank { get; set; }
+	[JsonProperty("timestamp")]
+	public DateTime Timestamp { get; set; }
+}
