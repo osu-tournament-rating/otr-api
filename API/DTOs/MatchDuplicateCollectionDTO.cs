38dbbea943b1873acde23a0b0161248ae10f7ced
hburn7 <hburnett777@gmail.com>
2024-01-29 20:37:18 -0500

    Move MatchDuplicateDTO to a new file

--- API/DTOs/MatchDuplicateCollectionDTO.cs
+++ API/DTOs/MatchDuplicateCollectionDTO.cs
@@ -2,15 +2,6 @@
 
 namespace API.DTOs;
 
-public class MatchDuplicateDTO
-{
-	public long OsuMatchId { get; set; }
-	public string Name { get; set; } = string.Empty!;
-	public bool? VerifiedAsDuplicate { get; set; }
-	public string? VerifiedByUsername { get; set; }
-	public int? VerifiedByUserId { get; set; }
-}
-
 public class MatchDuplicateCollectionDTO
 {
 	public int Id { get; set; }
---
+++ API/DTOs/MatchDuplicateDTO.cs
@@ -0,0 +1,10 @@
+namespace API.DTOs;
+
+public class MatchDuplicateDTO
+{
+	public long OsuMatchId { get; set; }
+	public string Name { get; set; } = string.Empty!;
+	public bool? VerifiedAsDuplicate { get; set; }
+	public string? VerifiedByUsername { get; set; }
+	public int? VerifiedByUserId { get; set; }
+}
