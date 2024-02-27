08bd3a5153034278cb24eecb4e0b2ca6acde9a89
Placni <pat.lacni@yahoo.com>
2024-02-27 17:44:44 -0600

    Fix possible null reference

--- API/Utilities/ConfigurationExtensions.cs
+++ API/Utilities/ConfigurationExtensions.cs
@@ -7,6 +7,10 @@ namespace API.Utilities
         public static T BindAndValidate<T>(this IConfiguration configuration, string sectionName) where T : new()
         {
             var section = configuration.GetSection(sectionName).Get<T>();
+            if (section == null)
+            {
+                throw new InvalidOperationException($"Section {sectionName} not found in configuration.");
+            }
 
             var context = new ValidationContext(section, serviceProvider: null, items: null);
             var validationResults = new List<ValidationResult>();
