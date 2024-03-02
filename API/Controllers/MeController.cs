be9592a309a2272d396c988217c186805607f94a
bugg86 <jackfink68@yahoo.com>
2024-03-02 18:51:33 -0500

    final cleanup

--- API/Controllers/MeController.cs
+++ API/Controllers/MeController.cs
@@ -4,7 +4,6 @@ using API.Utilities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
-using System.IdentityModel.Tokens.Jwt;
 using Asp.Versioning;
 
 namespace API.Controllers;
