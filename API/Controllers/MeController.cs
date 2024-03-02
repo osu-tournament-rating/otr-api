a33ea286fb540117766f8c7f360874047a0a417a
bugg86 <jackfink68@yahoo.com>
2024-03-02 18:48:36 -0500

    fixing mecontroller

--- API/Controllers/MeController.cs
+++ API/Controllers/MeController.cs
@@ -25,36 +25,36 @@ public class MeController : Controller
 		_playerStatsService = playerStatsService;
 	}
 
-	private bool IsWhitelisted(long osuId)
-	{
-		var whitelist = new long[]
-		{
-			11482346,
-			8191845,
-			11557554,
-			4001304,
-			6892711,
-			7153533,
-			3958619,
-			6701656,
-			1797189,
-			7802400,
-			11255340,
-			13175102,
-			11955929,
-			11292327,
-			18618027
-		};
+	// Will remove once all users are whitelisted in the database
+	// private bool IsWhitelisted(long osuId)
+	// {
+	// 	var whitelist = new long[]
+	// 	{
+	// 		11482346,
+	// 		8191845,
+	// 		11557554,
+	// 		4001304,
+	// 		6892711,
+	// 		7153533,
+	// 		3958619,
+	// 		6701656,
+	// 		1797189,
+	// 		7802400,
+	// 		11255340,
+	// 		13175102,
+	// 		11955929,
+	// 		11292327
+	// 	};
+	//
+	// 	return whitelist.Contains(osuId);
+	// }
 
-		return whitelist.Contains(osuId);
-	}
-	
 	[HttpGet]
 	[EndpointSummary("Gets the logged in user's information, if they exist")]
 	[ProducesResponseType<UserInfoDTO>(StatusCodes.Status200OK)]
 	[ProducesResponseType(StatusCodes.Status401Unauthorized)]
 	[ProducesResponseType(StatusCodes.Status404NotFound)]
-	public async Task<ActionResult<UserInfoDTO>> GetLoggedInUserAsync()
+	public async Task<IActionResult> GetLoggedInUserAsync()
 	{
 		int? id = HttpContext.AuthorizedUserIdentity();
 
@@ -63,15 +63,10 @@ public class MeController : Controller
 			return BadRequest("User is not logged in.");
 		}
 
-		var user = await _userService.GetForPlayerAsync(id.Value);
+		var user = await _userService.GetAsync(id.Value);
 		if (user?.OsuId == null)
 		{
 			return NotFound("User not found");
-		}
-
-		if (!IsWhitelisted(user.OsuId.Value))
-		{
-			return Unauthorized("User is not whitelisted");
 		}
 
 		return Ok(user);
@@ -82,65 +77,34 @@ public class MeController : Controller
 	/// </summary>
 	/// <returns></returns>
 	[HttpGet("validate")]
-	[EndpointSummary("Validates the currently logged in user's OTR-Access-Token cookie")]
-	[ProducesResponseType(StatusCodes.Status200OK)]
+	[Authorize(Roles = "whitelist")]
+	[EndpointSummary("Validates the currently logged in user has permissions to access the website.")]
 	[ProducesResponseType(StatusCodes.Status204NoContent)]
 	[ProducesResponseType(StatusCodes.Status400BadRequest)]
-	public async Task<IActionResult> ValidateJwt()
+	public IActionResult ValidateJwt()
 	{
-		if (!HttpContext.Request.Cookies.ContainsKey("OTR-Access-Token"))
-		{
-			return BadRequest("No access token cookie found.");
-		}
-
-		string? token = HttpContext.Request.Cookies["OTR-Access-Token"];
-		if (string.IsNullOrEmpty(token))
-		{
-			return BadRequest("Cookie found, but was null or empty.");
-		}
-
-		var loggedInUser = await GetLoggedInUserAsync();
-		if (loggedInUser.Result is UnauthorizedResult)
-		{
-			return Unauthorized("User is not whitelisted");
-		}
-
-		var jwtToken = new JwtSecurityTokenHandler().ReadJwtToken(token);
-		var expDate = jwtToken.ValidTo;
-		if (expDate < DateTime.UtcNow)
-		{
-			return NoContent();
-		}
-		
-		return Ok();
-	}
-
-	private int? GetId()
-	{
-		string? id = HttpContext.User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Name)?.Value;
-		if (id == null)
-		{
-			return null;
-		}
-
-		if (!int.TryParse(id, out int idInt))
-		{
-			return null;
-		}
-
-		return idInt;
+		// Middleware will return 403 if the user does not
+		// have the correct roles
+		return NoContent();
 	}
 
 	[HttpGet("stats")]
 	public async Task<ActionResult<PlayerStatsDTO>> GetStatsAsync([FromQuery] int mode = 0, [FromQuery] DateTime? dateMin = null, [FromQuery] DateTime? dateMax = null)
 	{
-		int? id = GetId();
+		int? userId = HttpContext.AuthorizedUserIdentity();
 
-		if (!id.HasValue)
+		if (!userId.HasValue)
 		{
 			return BadRequest("User is not logged in or id could not be retreived from logged in user.");
 		}
 
-		return await _playerStatsService.GetAsync(id.Value, null, mode, dateMin ?? DateTime.MinValue, dateMax ?? DateTime.UtcNow);
+		int? playerId = (await _userService.GetAsync(userId.Value))?.Id;
+
+		if (!playerId.HasValue)
+		{
+			return BadRequest("Unidentifiable user (unable to discern playerId).");
+		}
+
+		return await _playerStatsService.GetAsync(playerId.Value, null, mode, dateMin ?? DateTime.MinValue, dateMax ?? DateTime.UtcNow);
 	}
 }
