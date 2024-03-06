86346018c622d53101193dc8284d2b9bad56d030
hburn7 <hburnett777@gmail.com>
2024-03-06 08:52:32 -0500

    Removed unused ambiguous methods

--- API/Services/Interfaces/IPlayerService.cs
+++ API/Services/Interfaces/IPlayerService.cs
@@ -6,13 +6,6 @@ namespace API.Services.Interfaces;
 public interface IPlayerService
 {
     Task<IEnumerable<PlayerDTO>> GetAllAsync();
-    Task<PlayerInfoDTO?> GetByOsuIdAsync(
-        long osuId,
-        bool eagerLoad = false,
-        OsuEnums.Mode mode = OsuEnums.Mode.Standard,
-        int offsetDays = -1
-    );
-    Task<IEnumerable<PlayerInfoDTO>> GetByOsuIdsAsync(IEnumerable<long> osuIds);
     Task<IEnumerable<PlayerRanksDTO>> GetAllRanksAsync();
     Task<IEnumerable<PlayerRatingDTO>> GetTopRatingsAsync(int n, OsuEnums.Mode mode);
     Task<string?> GetUsernameAsync(long osuId);
