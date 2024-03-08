b90c2aca98e00e87e18a74f9d16e9132aa754775
hburn7 <hburnett777@gmail.com>
2024-03-08 13:50:01 -0500

    Add versatile player info fetching

--- API/Controllers/PlayersController.cs
+++ API/Controllers/PlayersController.cs
@@ -30,29 +30,17 @@ public class PlayersController : Controller
         return Ok(players);
     }
 
-    [HttpGet("{osuId:long}/info")]
-    public async Task<ActionResult<PlayerInfoDTO?>> GetByUserIdAsync(long osuId)
+    [HttpGet("{key}/info")]
+    public async Task<ActionResult<PlayerInfoDTO?>> GetAsync(string key)
     {
-        var info = await _playerService.GetAsync(osuId);
+        var info = await _playerService.GetVersatileAsync(key);
 
         if (info == null)
         {
-            return NotFound($"User with osuid {osuId} does not exist");
+            return NotFound($"User with key {key} does not exist");
         }
 
         return info;
-    }
-
-    [HttpGet("{username}/info")]
-    public async Task<ActionResult<PlayerInfoDTO?>> GetByUserIdAsync(string username)
-    {
-        var player = await _playerService.GetAsync(username);
-        if (player != null)
-        {
-            return Ok(player);
-        }
-
-        return NotFound($"User with username {username} does not exist");
     }
 
     [HttpGet("ranks/all")]
--- API/Services/Interfaces/IPlayerService.cs
+++ API/Services/Interfaces/IPlayerService.cs
@@ -20,6 +20,20 @@ public interface IPlayerService
     Task<IEnumerable<PlayerIdMappingDTO>> GetIdMappingAsync();
 
     Task<IEnumerable<PlayerCountryMappingDTO>> GetCountryMappingAsync();
+    /// <summary>
+    /// Dynamically searches for players via the following, in order of priority:
+    ///
+    /// If the key can be parsed as an integer:
+    /// - The player id
+    /// - The osu id
+    ///
+    /// Searches for the username (case insensitive) otherwise.
+    ///
+    /// Returns null if no matches found.
+    /// </summary>
+    /// <param name="key">The dynamic key of the player to look for</param>
+    /// <returns></returns>
+    Task<PlayerInfoDTO?> GetVersatileAsync(string key);
     Task<PlayerInfoDTO?> GetAsync(int userId);
     Task<PlayerInfoDTO?> GetAsync(long osuId);
     Task<PlayerInfoDTO?> GetAsync(string username);
