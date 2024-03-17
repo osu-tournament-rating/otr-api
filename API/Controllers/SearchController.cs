6290c4a7baf3af73a799ee6b5524869b4d40cd59
bugg86 <jackfink68@yahoo.com>
2024-03-17 14:16:24 -0400

    refactored to use TypedResults

--- API/Controllers/SearchController.cs
+++ API/Controllers/SearchController.cs
@@ -3,6 +3,7 @@ using API.Services.Interfaces;
 using Asp.Versioning;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -18,11 +19,9 @@ public class SearchController(ISearchService service) : Controller
 
     [HttpGet]
     [EndpointSummary("Allows for partial or full searching on the names of tournaments, matches and usernames.")]
-    [ProducesResponseType<SearchResponseDTO>(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> SearchByNames([FromQuery] string? tournamentName, [FromQuery] string? matchName, [FromQuery] string? username)
+    public async Task<Results<NotFound<string>, Ok<List<SearchResponseDTO>>>> SearchByNames([FromQuery] string? tournamentName, [FromQuery] string? matchName, [FromQuery] string? username)
     {
         List<SearchResponseDTO>? response = await _searchService.SearchByNameAsync(tournamentName, matchName, username);
-        return response is null ? NotFound("Requested search returned nothing.") : Ok(response);
+        return response is null ? TypedResults.NotFound("Requested search returned nothing.") : TypedResults.Ok(response);
     }
 }
