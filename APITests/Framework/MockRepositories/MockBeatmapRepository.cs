7ce83e187a7b6d6e646c0dc3220cb9673abd06e8
hburn7 <hburnett777@gmail.com>
2024-03-23 11:37:23 -0400

    Add MockBeatmapRepository

---
+++ APITests/Framework/MockRepositories/MockBeatmapRepository.cs
@@ -0,0 +1,39 @@
+using API.Entities;
+using API.Repositories.Interfaces;
+using APITests.SeedData;
+using Moq;
+
+namespace APITests.Framework.MockRepositories;
+
+public class MockBeatmapRepository : Mock<IBeatmapRepository>
+{
+    public MockBeatmapRepository SetupAll() =>
+        SetupGet()
+            .SetupGetId();
+
+    public MockBeatmapRepository SetupGet()
+    {
+        Setup(x => x.GetAsync(It.IsAny<int>()))
+            .ReturnsAsync((int id) =>
+            {
+                Beatmap beatmap = SeededBeatmap.Get();
+                beatmap.Id = id;
+
+                return beatmap;
+            });
+
+        return this;
+    }
+
+    public MockBeatmapRepository SetupGetId()
+    {
+        Setup(x => x.GetIdAsync(It.IsAny<long>()))
+            .ReturnsAsync(() =>
+            {
+                Beatmap beatmap = SeededBeatmap.Get();
+                return beatmap.Id;
+            });
+
+        return this;
+    }
+}
