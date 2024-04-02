b94284d4381d710ff11052e7563384f50d53d15c
hburn7 <hburnett777@gmail.com>
2024-04-01 21:02:23 -0400

    remove verbose checks

--- API/Controllers/ScreeningController.cs
+++ API/Controllers/ScreeningController.cs
@@ -2,63 +2,42 @@ using API.DTOs;
 using API.Services.Interfaces;
 using Asp.Versioning;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 [ApiController]
 [ApiVersion(1)]
-[EnableCors]
 [Route("api/v{version:apiVersion}/[controller]")]
 public class ScreeningController(IScreeningService screeningService) : Controller
 {
+    /// <summary>
+    /// Screen a list of users based on the criteria as described in
+    /// <see cref="ScreeningResultDTO"/>
+    ///
+    /// <remarks>
+    ///
+    /// This is a test remark
+    ///
+    /// </remarks>
+    /// </summary>
+    /// <param name="screeningRequest"></param>
+    /// <returns></returns>
     [HttpPost]
     [Authorize(Roles = "user, client")]
-    [EndpointSummary("Allows authorized users & clients to submit a collection of " +
-                     "users to be screened based on a set of criteria")]
-    public async Task<Results<Ok<IEnumerable<ScreeningResultDTO>>, BadRequest<string>>> ScreenAsync([FromBody] ScreeningDTO screeningRequest)
+    [ProducesResponseType(typeof(ScreeningResultDTO), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> ScreenAsync([FromBody] ScreeningRequestDTO screeningRequest)
     {
-        if (!screeningRequest.OsuPlayerIds.Any())
+        if (!ModelState.IsValid)
         {
-            return TypedResults.BadRequest("Screening list cannot be empty");
-        }
-
-        if (screeningRequest.OsuPlayerIds.Count() > 1000)
-        {
-            return TypedResults.BadRequest("Screening list cannot exceed 1000 players");
-        }
-
-        if (screeningRequest.Ruleset is < 0 or > 3)
-        {
-            return TypedResults.BadRequest("Invalid ruleset (must be 0-3)");
-        }
-
-        if (screeningRequest.MatchesPlayed < 0)
-        {
-            return TypedResults.BadRequest("Matches played must be greater than or equal to 0");
-        }
-
-        if (screeningRequest.MinRating < 100)
-        {
-            return TypedResults.BadRequest("Minimum rating must be greater than or equal to 100");
-        }
-
-        if (screeningRequest.MaxRating < 100)
-        {
-            return TypedResults.BadRequest("Maximum rating must be greater than or equal to 100");
-        }
-
-        if (screeningRequest.MinRating >= screeningRequest.MaxRating)
-        {
-            return TypedResults.BadRequest("Minimum rating must be less than maximum rating");
+            return BadRequest(ModelState);
         }
 
         // Filter out duplicate ids
         screeningRequest.OsuPlayerIds = screeningRequest.OsuPlayerIds.Distinct();
 
         IEnumerable<ScreeningResultDTO> results = await screeningService.ScreenAsync(screeningRequest);
-        return TypedResults.Ok(results);
+        return Ok(results);
     }
 }
