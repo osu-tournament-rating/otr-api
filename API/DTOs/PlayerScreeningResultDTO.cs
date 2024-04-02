83613f1e453fd745f9905f1f480d5284d345b3f9
hburn7 <hburnett777@gmail.com>
2024-04-01 21:38:35 -0400

    Move screening enums to enums folder

--- API/DTOs/PlayerScreeningResultDTO.cs
+++ API/DTOs/PlayerScreeningResultDTO.cs
@@ -1,61 +1,6 @@
+using API.Enums;
+
 namespace API.DTOs;
-
-/// <summary>
-/// Indicates whether a player passed or failed screening
-/// </summary>
-public enum ScreeningResult
-{
-    /// <summary>
-    /// The player passed screening
-    /// </summary>
-    Pass,
-    /// <summary>
-    /// The player failed screening
-    /// </summary>
-    Fail
-}
-
-/// <summary>
-/// Explains why the player failed screening
-/// </summary>
-[Flags]
-public enum ScreeningFailReason
-{
-    /// <summary>
-    /// The player passed screening, thus there is no failure reason
-    /// </summary>
-    None = 0,
-    /// <summary>
-    /// This player does not have a rating / profile in our system
-    /// </summary>
-    NoData = 1 << 0,
-    /// <summary>
-    /// The player's rating is below the minimum threshold
-    /// </summary>
-    MinRating = 1 << 1,
-    /// <summary>
-    /// The player's rating is above the maximum threshold
-    /// </summary>
-    MaxRating = 1 << 2,
-    /// <summary>
-    /// The player is provisional and the screening criteria specifies
-    /// exclusion of provisional players
-    /// </summary>
-    IsProvisional = 1 << 3,
-    /// <summary>
-    /// The player has not played in the minimum specified
-    /// amount of tournaments
-    /// </summary>
-    NotEnoughTournaments = 1 << 4,
-    /// <summary>
-    /// The player's all-time peak rating is above the maximum allowed
-    /// </summary>
-    PeakRatingTooHigh = 1 << 5,
-    /// <summary>
-    /// The player has not played in the minimum specified amount of matches
-    /// </summary>
-    NotEnoughMatches = 1 << 6
-}
 
 /// <summary>
 /// Represents one player's screening result
