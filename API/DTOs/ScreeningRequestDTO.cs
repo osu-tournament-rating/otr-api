1d06339718237f1281cec23d0b4c75a37d4bb38a
hburn7 <hburnett777@gmail.com>
2024-04-01 21:01:47 -0400

    Add data annotations to ScreeningRequestDTO

--- API/DTOs/ScreeningRequestDTO.cs
+++ API/DTOs/ScreeningRequestDTO.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using API.Controllers;
 
 namespace API.DTOs;
@@ -7,19 +8,24 @@ namespace API.DTOs;
 /// determine what types of players are eligible/ineligible
 /// to play in a tournament that screens by criteria here.
 /// </summary>
-public class ScreeningDTO
+public class ScreeningRequestDTO
 {
     /// <summary>
     /// The ruleset by which data will be referenced, required
     /// </summary>
+    [Required]
+    [Range(0, 3, ErrorMessage = "Invalid ruleset (0 = osu!, " +
+                                "1 = osu!Taiko, 2 = osu!Catch, 3 = osu!Mania)")]
     public int Ruleset { get; set; }
     /// <summary>
     /// Players with a current rating below this value will be screened
     /// </summary>
+    [Range(100, int.MaxValue, ErrorMessage = "Minimum rating value is 100")]
     public int? MinRating { get; set; }
     /// <summary>
     /// Players with a current rating above this value will be screened
     /// </summary>
+    [Range(100, int.MaxValue, ErrorMessage = "Minimum rating value is 100")]
     public int? MaxRating { get; set; }
     /// <summary>
     /// Whether to consider players that currently have a provisional rating
@@ -29,19 +35,23 @@ public class ScreeningDTO
     /// If set, requires players to have participated in at least
     /// this many distinct tournaments
     /// </summary>
+    [Range(1, int.MaxValue, ErrorMessage = "Tournaments played must be at least 1.")]
     public int? TournamentsPlayed { get; set; }
     /// <summary>
     /// If set, requires players to have an all-time peak rating less than
     /// this value
     /// </summary>
+    [Range(100, int.MaxValue, ErrorMessage = "Minimum rating value is 100")]
     public int? PeakRating { get; set; }
     /// <summary>
     /// If set, requires players to have played in at least
     /// this many matches
     /// </summary>
+    [Range(1, int.MaxValue, ErrorMessage = "Matches played must be at least 1.")]
     public int? MatchesPlayed { get; set; }
     /// <summary>
     /// A list of osu! player ids that will be screened
     /// </summary>
+    [Required]
     public IEnumerable<long> OsuPlayerIds { get; set; } = new List<long>();
 }
