be75219720873b1465c1452f33593638a4b092b4
myssto <pat.lacni@yahoo.com>
2024-04-04 22:54:43 -0500

    Update existing usages of CreatedAtRouteValues

--- API/Utilities/GenerateLocationUriAction.cs
+++ API/Utilities/GenerateLocationUriAction.cs
@@ -1,31 +1,16 @@
 using System.Diagnostics.CodeAnalysis;
-using API.DTOs;
 using API.DTOs.Interfaces;
 using API.Entities.Interfaces;
+using API.Services.Interfaces;
 using AutoMapper;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.AspNetCore.Mvc.Routing;
 
 namespace API.Utilities;
 
 [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
-public class GenerateLocationUriAction(IUrlHelperFactory urlHelperFactory, IActionContextAccessor actionContextAccessor)
-    : IMappingAction<IEntity, ICreatedResult>
+public class GenerateLocationUriAction(IUrlHelperService urlHelperService) : IMappingAction<IEntity, ICreatedResult>
 {
-    private readonly IUrlHelperFactory _urlHelperFactory = urlHelperFactory;
-    private readonly IActionContextAccessor _actionContextAccessor = actionContextAccessor;
-
     public void Process(IEntity src, ICreatedResult dest, ResolutionContext ctx)
     {
-        if (_actionContextAccessor.ActionContext is null)
-        {
-            dest.Location = "unknown";
-            return;
-        }
-
-        IUrlHelper urlHelper = _urlHelperFactory.GetUrlHelper(_actionContextAccessor.ActionContext);
-        CreatedAtRouteValues routeValues = dest.CreatedAtRouteValues;
-        dest.Location = urlHelper.Action(routeValues.Action, routeValues.Controller, routeValues.RouteValues) ?? "unknown";
+        dest.Location = urlHelperService.Action(dest.CreatedAtRouteValues);
     }
 }
