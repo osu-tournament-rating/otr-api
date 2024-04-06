f3098e18262b55357db23623af795b2e52ee5ddb
myssto <pat.lacni@yahoo.com>
2024-04-06 16:14:50 -0500

    Use strict null checks to avoid exceptions

--- API/Services/Implementations/UrlHelperService.cs
+++ API/Services/Implementations/UrlHelperService.cs
@@ -8,13 +8,43 @@ namespace API.Services.Implementations;
 
 public class UrlHelperService(IUrlHelperFactory urlHelperFactory, IActionContextAccessor actionContextAccessor) : IUrlHelperService
 {
+    private readonly HttpRequest? _httpRequest = actionContextAccessor.ActionContext?.HttpContext.Request;
+
     private readonly IUrlHelper? _urlHelper = actionContextAccessor.ActionContext != null
         ? urlHelperFactory.GetUrlHelper(actionContextAccessor.ActionContext)
         : null;
 
-    public string Action(string action, string controller, object routeValues) =>
-        _urlHelper?.Action(action, controller, routeValues) ?? "unknown";
+    private const string UnknownText = "unknown";
 
-    public string Action(CreatedAtRouteValues routeValues) =>
-        _urlHelper?.Action(routeValues.Action, routeValues.Controller, routeValues.RouteValues) ?? "unknown";
+    public string Action(string action, string controller, object routeValues)
+    {
+        if (_urlHelper is null || _httpRequest is null)
+        {
+            return UnknownText;
+        }
+
+        return _urlHelper.Action(
+            action,
+            controller,
+            routeValues,
+            _httpRequest.Scheme,
+            _httpRequest.Host.ToUriComponent()
+        ) ?? UnknownText;
+    }
+
+    public string Action(CreatedAtRouteValues routeValues)
+    {
+        if (_urlHelper is null || _httpRequest is null)
+        {
+            return UnknownText;
+        }
+
+        return _urlHelper.Action(
+            routeValues.Action,
+            routeValues.Controller,
+            routeValues.RouteValues,
+            _httpRequest.Scheme,
+            _httpRequest.Host.ToUriComponent()
+        ) ?? UnknownText;
+    }
 }
