b585722acd1d2f4bb7bbd53e574b66e7375360d4
myssto <pat.lacni@yahoo.com>
2024-04-07 14:05:51 -0500

    Use primary constructor

--- API/Services/Implementations/TournamentsService.cs
+++ API/Services/Implementations/TournamentsService.cs
@@ -9,16 +9,13 @@ namespace API.Services.Implementations;
 
 public class TournamentsService(ITournamentsRepository tournamentsRepository, IMapper mapper) : ITournamentsService
 {
-    private readonly ITournamentsRepository _tournamentsRepository = tournamentsRepository;
-    private readonly IMapper _mapper = mapper;
-
     public async Task<TournamentCreatedResultDTO> CreateAsync(TournamentWebSubmissionDTO wrapper, bool verify, int? verificationSource)
     {
         MatchVerificationStatus verificationStatus = verify
             ? MatchVerificationStatus.Verified
             : MatchVerificationStatus.PendingVerification;
 
-        Tournament tournament = await _tournamentsRepository.CreateAsync(new Tournament
+        Tournament tournament = await tournamentsRepository.CreateAsync(new Tournament
         {
             Name = wrapper.TournamentName,
             Abbreviation = wrapper.Abbreviation,
@@ -38,34 +35,34 @@ public class TournamentsService(ITournamentsRepository tournamentsRepository, IM
                 SubmitterUserId = wrapper.SubmitterId
             }).ToList()
         });
-        return _mapper.Map<TournamentCreatedResultDTO>(tournament);
+        return mapper.Map<TournamentCreatedResultDTO>(tournament);
     }
 
-    public async Task<bool> ExistsAsync(int id) => await _tournamentsRepository.ExistsAsync(id);
+    public async Task<bool> ExistsAsync(int id) => await tournamentsRepository.ExistsAsync(id);
 
-    public async Task<bool> ExistsAsync(string name, int mode) => await _tournamentsRepository.ExistsAsync(name, mode);
+    public async Task<bool> ExistsAsync(string name, int mode) => await tournamentsRepository.ExistsAsync(name, mode);
 
     public async Task<IEnumerable<TournamentDTO>> ListAsync()
     {
-        IEnumerable<Tournament> items = await _tournamentsRepository.GetAllAsync();
+        IEnumerable<Tournament> items = await tournamentsRepository.GetAllAsync();
         items = items.OrderBy(x => x.Name);
 
-        return _mapper.Map<IEnumerable<TournamentDTO>>(items);
+        return mapper.Map<IEnumerable<TournamentDTO>>(items);
     }
 
     public async Task<TournamentDTO?> GetAsync(int id, bool eagerLoad = true) =>
-        _mapper.Map<TournamentDTO>(await _tournamentsRepository.GetAsync(id, eagerLoad));
+        mapper.Map<TournamentDTO>(await tournamentsRepository.GetAsync(id, eagerLoad));
 
     public async Task<int> CountPlayedAsync(
         int playerId,
         int mode,
         DateTime? dateMin = null,
         DateTime? dateMax = null
-    ) => await _tournamentsRepository.CountPlayedAsync(playerId, mode, dateMin ?? DateTime.MinValue, dateMax ?? DateTime.MaxValue);
+    ) => await tournamentsRepository.CountPlayedAsync(playerId, mode, dateMin ?? DateTime.MinValue, dateMax ?? DateTime.MaxValue);
 
     public async Task<TournamentDTO?> UpdateAsync(int id, TournamentDTO wrapper)
     {
-        Tournament? existing = await _tournamentsRepository.GetAsync(id);
+        Tournament? existing = await tournamentsRepository.GetAsync(id);
         if (existing is null)
         {
             return null;
@@ -78,7 +75,7 @@ public class TournamentsService(ITournamentsRepository tournamentsRepository, IM
         existing.RankRangeLowerBound = wrapper.RankRangeLowerBound;
         existing.TeamSize = wrapper.TeamSize;
 
-        await _tournamentsRepository.UpdateAsync(existing);
-        return _mapper.Map<TournamentDTO>(existing);
+        await tournamentsRepository.UpdateAsync(existing);
+        return mapper.Map<TournamentDTO>(existing);
     }
 }
