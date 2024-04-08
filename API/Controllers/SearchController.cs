b278ae374b9c1dad8bc8b21e1078fb16c82b629f
myssto <pat.lacni@yahoo.com>
2024-04-08 17:18:57 -0500

    Use single query for search

--- API/Controllers/SearchController.cs
+++ API/Controllers/SearchController.cs
@@ -2,26 +2,30 @@ using API.DTOs;
 using API.Services.Interfaces;
 using Asp.Versioning;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 [ApiController]
 [ApiVersion(1)]
-[EnableCors]
 [Authorize(Roles = "system")] // Internal access only at this time
 [Route("api/v{version:apiVersion}/[controller]")]
 public class SearchController(ISearchService service) : Controller
 {
-    private readonly ISearchService _searchService = service;
-
+    /// <summary>
+    /// Search for tournaments, matches, and users
+    /// </summary>
+    /// <remarks>
+    /// Allows for partial or full searching on the names of tournaments, matches, and usernames
+    /// </remarks>
+    /// <param name="searchKey">The string to match against names of tournaments, matches, and usernames</param>
+    /// <response code="404">If no data matches the given search key</response>
+    /// <response code="200">Returns a list of all possible tournaments, matches, and usernames for the given search key</response>
     [HttpGet]
     [EndpointSummary("Allows for partial or full searching on the names of tournaments, matches and usernames.")]
-    public async Task<Results<NotFound, Ok<List<SearchResponseDTO>>>> SearchByNamesAsync([FromQuery] string? tournamentName, [FromQuery] string? matchName, [FromQuery] string? username)
+    public async Task<IActionResult> SearchByNamesAsync([FromQuery] string searchKey)
     {
-        List<SearchResponseDTO>? response = await _searchService.SearchByNameAsync(tournamentName, matchName, username);
-        return response is null ? TypedResults.NotFound() : TypedResults.Ok(response);
+        List<SearchResponseDTO> response = await service.SearchByNameAsync(searchKey);
+        return response.Count == 0 ? NotFound() : Ok(response);
     }
 }
