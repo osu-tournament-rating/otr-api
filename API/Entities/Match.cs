f09f311a100efa850d53135237f9de0b7bf9143b
myssto <pat.lacni@yahoo.com>
2024-04-15 00:41:45 -0500

    Add XML docs

--- API/Entities/Match.cs
+++ API/Entities/Match.cs
@@ -1,34 +1,73 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 using API.Entities.Interfaces;
 using API.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Entities;
 
+/// <summary>
+/// Represents a tournament match
+/// </summary>
 [Table("matches")]
+// ReSharper disable once StringLiteralTypo
 [Index("MatchId", Name = "osumatches_matchid", IsUnique = true)]
+[SuppressMessage("ReSharper", "ClassWithVirtualMembersNeverInherited.Global")]
+[SuppressMessage("ReSharper", "EntityFramework.ModelValidation.CircularDependency")]
+[SuppressMessage("ReSharper", "PropertyCanBeMadeInitOnly.Global")]
 public class Match : MatchEntityBase, IUpdateableEntity
 {
+    /// <summary>
+    /// Date the entity was created
+    /// </summary>
     [Column("created", TypeName = "timestamp with time zone")]
     public DateTime Created { get; set; }
 
+    /// <summary>
+    /// Date of the last update to the entity
+    /// </summary>
     [Column("updated", TypeName = "timestamp with time zone")]
     public DateTime? Updated { get; set; }
+
+    /// <summary>
+    /// The user that submitted the match
+    /// </summary>
+    [InverseProperty("SubmittedMatches")]
     public virtual User? SubmittedBy { get; set; }
+
+    /// <summary>
+    /// The user that verified the match
+    /// </summary>
+    [InverseProperty("VerifiedMatches")]
     public virtual User? VerifiedBy { get; set; }
 
+    /// <summary>
+    /// All games played during the match
+    /// </summary>
     [InverseProperty("Match")]
     public virtual ICollection<Game> Games { get; set; } = new List<Game>();
 
+    /// <summary>
+    /// The tournament that the match was played in
+    /// </summary>
     [InverseProperty("Matches")]
     public virtual Tournament Tournament { get; set; } = null!;
 
+    /// <summary>
+    /// All player stats for the match
+    /// </summary>
     [InverseProperty("Match")]
     public virtual ICollection<PlayerMatchStats> Stats { get; set; } = new List<PlayerMatchStats>();
 
+    /// <summary>
+    /// All rating stats for the match
+    /// </summary>
     [InverseProperty("Match")]
     public virtual ICollection<MatchRatingStats> RatingStats { get; set; } = new List<MatchRatingStats>();
 
+    /// <summary>
+    /// The win record for the match
+    /// </summary>
     [InverseProperty("Match")]
     public virtual MatchWinRecord WinRecord { get; set; } = new();
 
--- API/Entities/MatchEntityBase.cs
+++ API/Entities/MatchEntityBase.cs
@@ -1,58 +1,90 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 namespace API.Entities;
 
 public class MatchEntityBase
 {
+    /// <summary>
+    /// Primary key
+    /// </summary>
     [Key]
     [Column("id")]
     public int Id { get; set; }
 
     /// <summary>
-    ///  The osu! match id
+    /// The osu! match id
     /// </summary>
     [Column("match_id")]
     public long MatchId { get; set; }
 
+    /// <summary>
+    /// Name of the lobby the match was played in
+    /// </summary>
     [Column("name")]
     [MaxLength(512)]
     public string? Name { get; set; }
 
+    /// <summary>
+    /// Timestamp of the beginning of the match
+    /// </summary>
     [Column("start_time", TypeName = "timestamp with time zone")]
     public DateTime? StartTime { get; set; }
 
+    /// <summary>
+    /// Timestamp of the end of the match
+    /// </summary>
     [Column("end_time", TypeName = "timestamp with time zone")]
     public DateTime? EndTime { get; set; }
 
+    /// <summary>
+    /// Any additional information included when <see cref="VerificationStatus"/> changes
+    /// </summary>
     [Column("verification_info")]
     [MaxLength(512)]
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
     public string? VerificationInfo { get; set; }
 
+    /// <summary>
+    /// The type of source that verified the match
+    /// </summary>
     [Column("verification_source")]
     public int? VerificationSource { get; set; }
 
+    /// <summary>
+    /// The verification status of the match
+    /// </summary>
     [Column("verification_status")]
     public int? VerificationStatus { get; set; }
 
+    /// <summary>
+    /// The id of the user that verified the match
+    /// </summary>
     [Column("verified_by_user")]
     public int? VerifierUserId { get; set; }
 
+    /// <summary>
+    /// The id of the tournament the match was played in
+    /// </summary>
     [Column("tournament_id")]
     public int TournamentId { get; set; }
 
     /// <summary>
-    /// Checked by a background worker to see if the match needs to be processed.
+    /// Indicates to background workers that the match needs to be processed
     /// </summary>
     [Column("needs_auto_check")]
     public bool? NeedsAutoCheck { get; set; }
 
     /// <summary>
-    /// A flag indicating whether this match needs to be processed by the API.
+    /// Indicates to background workers that the match needs to be processed with the osu! api
     /// </summary>
     [Column("is_api_processed")]
     public bool? IsApiProcessed { get; set; }
 
+    /// <summary>
+    /// The id of the user that submitted the match
+    /// </summary>
     [Column("submitted_by_user")]
     public int? SubmitterUserId { get; set; }
 }
