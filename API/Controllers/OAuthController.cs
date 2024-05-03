9ee94eb92128f221c752dc5b71e3b795273beb59
myssto <pat.lacni@yahoo.com>
2024-05-03 17:01:46 -0500

    Add docs to OAuthController

--- API/Controllers/OAuthController.cs
+++ API/Controllers/OAuthController.cs
@@ -3,7 +3,6 @@ using API.Handlers.Interfaces;
 using API.Utilities;
 using Asp.Versioning;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -14,64 +13,84 @@ namespace API.Controllers;
 [Route("api/v{version:apiVersion}/[controller]")]
 public class OAuthController(IOAuthHandler oAuthHandler) : Controller
 {
-    private readonly IOAuthHandler _oAuthHandler = oAuthHandler;
-
+    /// <summary>
+    /// Authorize using an osu! authorization code
+    /// </summary>
+    /// <param name="code">The osu! authorization code</param>
+    /// <response code="201">If there was an error during authorization</response>
+    /// <response code="200">Returns user access credentials</response>
     [HttpPost("authorize")]
-    [EndpointDescription("Authorize using an osu! OAuth code")]
-    public async Task<Results<UnauthorizedHttpResult, Ok<OAuthResponseDTO>>> AuthorizeAsync([FromQuery] string code)
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType<OAuthResponseDTO>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> AuthorizeAsync([FromQuery] string code)
     {
-        OAuthResponseDTO? result = await _oAuthHandler.AuthorizeAsync(code);
+        OAuthResponseDTO? result = await oAuthHandler.AuthorizeAsync(code);
 
-        if (result == null)
-        {
-            return TypedResults.Unauthorized();
-        }
-
-        return TypedResults.Ok(result);
+        return result is not null
+            ? Ok(result)
+            : Unauthorized();
     }
 
+    /// <summary>
+    /// Authorize using client credentials
+    /// </summary>
+    /// <param name="clientId">The id of the client</param>
+    /// <param name="clientSecret">The secret of the client</param>
+    /// <response code="201">If there was an error during authorization</response>
+    /// <response code="200">Returns client access credentials</response>
     [HttpPost("token")]
-    [EndpointDescription("Obtain an access and refresh token using OAuth credentials")]
-    public async Task<Results<UnauthorizedHttpResult, Ok<OAuthResponseDTO>>> AuthorizeAsync([FromQuery] int clientId, [FromQuery] string clientSecret)
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType<OAuthResponseDTO>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> AuthorizeClientAsync([FromQuery] int clientId, [FromQuery] string clientSecret)
     {
-        OAuthResponseDTO? authorizationResponse = await _oAuthHandler.AuthorizeAsync(clientId, clientSecret);
+        OAuthResponseDTO? result = await oAuthHandler.AuthorizeAsync(clientId, clientSecret);
 
-        if (authorizationResponse == null)
-        {
-            return TypedResults.Unauthorized();
-        }
-
-        return TypedResults.Ok(authorizationResponse);
+        return result is not null
+            ? Ok(result)
+            : Unauthorized();
     }
 
+    /// <summary>
+    /// Create a new OAuth client
+    /// </summary>
+    /// <response code="201">If the user is not properly authenticated</response>
+    /// <response code="200">Returns created client credentials</response>
     [HttpPost("client")]
     [Authorize(Roles = "user")]
-    [EndpointDescription("Create a new OAuth client")]
-    public async Task<Results<UnauthorizedHttpResult, Ok<OAuthClientDTO>>> CreateClientAsync()
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType<OAuthClientDTO>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> CreateClientAsync()
     {
-        var userId = HttpContext.AuthorizedUserIdentity();
-
+        var userId = User.AuthorizedIdentity();
         if (!userId.HasValue)
         {
-            return TypedResults.Unauthorized();
+            return Unauthorized();
         }
 
-        OAuthClientDTO result = await _oAuthHandler.CreateClientAsync(userId.Value);
+        OAuthClientDTO result = await oAuthHandler.CreateClientAsync(userId.Value);
 
-        return TypedResults.Ok(result);
+        return Ok(result);
     }
 
+    /// <summary>
+    /// Generate new access credentials from a valid refresh token
+    /// </summary>
+    /// <remarks>
+    /// Generated access credentials will contain only a new access token,
+    /// and the given refresh token is returned with it
+    /// </remarks>
+    /// <param name="refreshToken"></param>
+    /// <response code="201">If the given refresh token is invalid, or there was an error during authorization</response>
+    /// <response code="200">Returns access credentials containing a new access token</response>
     [HttpPost("refresh")]
-    [EndpointDescription("Generate new OAuth credentials from a valid refresh token")]
-    public async Task<Results<UnauthorizedHttpResult, Ok<OAuthResponseDTO>>> RefreshAsync([FromQuery] string refreshToken)
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType<OAuthResponseDTO>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> RefreshAsync([FromQuery] string refreshToken)
     {
-        OAuthResponseDTO? result = await _oAuthHandler.RefreshAsync(refreshToken);
+        OAuthResponseDTO? result = await oAuthHandler.RefreshAsync(refreshToken);
 
-        if (result == null)
-        {
-            return TypedResults.Unauthorized();
-        }
-
-        return TypedResults.Ok(result);
+        return result is not null
+            ? Ok(result)
+            : Unauthorized();
     }
 }
