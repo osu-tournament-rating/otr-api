252ce6e87684c2ba5bc3f2003b10514de0f231cf
myssto <pat.lacni@yahoo.com>
2024-05-08 03:23:59 -0500

    Documentation for CacheUtils and CacheHandler

--- API/Handlers/Interfaces/ICacheHandler.cs
+++ API/Handlers/Interfaces/ICacheHandler.cs
@@ -5,9 +5,18 @@ namespace API.Handlers.Interfaces;
 
 public interface ICacheHandler : IContext
 {
+    /// <summary>
+    /// Adds a list of tournament search results to the cache, indexed by the given query
+    /// </summary>
     void SetTournamentSearchResult(IEnumerable<TournamentSearchResultDTO> result, string query);
 
+    /// <summary>
+    /// Adds a list of match search results to the cache, indexed by the given query
+    /// </summary>
     void SetMatchSearchResult(IEnumerable<MatchSearchResultDTO> result, string query);
 
+    /// <summary>
+    /// Adds a list of player search results to the cache, indexed by the given query
+    /// </summary>
     void SetPlayerSearchResult(IEnumerable<PlayerSearchResultDTO> result, string query);
 }
