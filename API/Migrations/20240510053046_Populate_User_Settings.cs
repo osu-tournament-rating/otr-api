0970d8bd777a12bb57dfdf1bfd70383435e29472
myssto <pat.lacni@yahoo.com>
2024-05-10 21:57:16 -0500

    Populate settings for existing users

---
+++ API/Migrations/20240510053046_Populate_User_Settings.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Populate_User_Settings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Populate user settings with the default ruleset of their respective player
+            // entry by default and use Ruleset.Standard as a fallback
+            migrationBuilder.Sql(
+                @"
+                INSERT INTO user_settings (user_id, default_ruleset)
+                SELECT u.id, coalesce(p.default_ruleset, 0)
+                FROM users u
+                JOIN players p ON p.id = u.player_id
+                "
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
