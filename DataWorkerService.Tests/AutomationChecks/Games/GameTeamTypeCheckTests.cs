b8b9b2e8cb3beebe950184e03ecfb31ebedf4a96
myssto <pat.lacni@yahoo.com>
2024-07-04 23:53:12 -0500

    DataWorkerService.Tests finish game automation checks tests

---
+++ DataWorkerService.Tests/AutomationChecks/Games/GameTeamTypeCheckTests.cs
@@ -0,0 +1,32 @@
+using Database.Entities;
+using Database.Enums;
+using Database.Enums.Verification;
+using DataWorkerService.AutomationChecks.Games;
+using TestingUtils.SeededData;
+
+namespace DataWorkerService.Tests.AutomationChecks.Games;
+
+public class GameTeamTypeCheckTests : AutomationChecksTestBase<GameTeamTypeCheck>
+{
+    [Theory]
+    [InlineData(TeamType.HeadToHead, false)]
+    [InlineData(TeamType.TagCoop, false)]
+    [InlineData(TeamType.TeamVs, true)]
+    [InlineData(TeamType.TagTeamVs, false)]
+    public void Check_PassesWhenExpected(TeamType teamType, bool expectedPass)
+    {
+        // Arrange
+        Game game = SeededGame.Generate(teamType: teamType, rejectionReason: GameRejectionReason.None);
+
+        GameRejectionReason expectedRejectionReason = expectedPass
+            ? GameRejectionReason.None
+            : GameRejectionReason.InvalidTeamType;
+
+        // Act
+        var actualPass = AutomationCheck.Check(game);
+
+        // Assert
+        Assert.Equal(expectedPass, actualPass);
+        Assert.Equal(expectedRejectionReason, game.RejectionReason);
+    }
+}
