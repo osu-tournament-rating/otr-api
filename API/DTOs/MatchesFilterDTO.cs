afb73abf9f01348afba4214d77d5a488d54310f7
Placni <pat.lacni@yahoo.com>
2024-08-28 12:03:53 -0500

    API add documentation to MatchesQuerySortType and MatchesFilterDTO

--- API/DTOs/MatchesFilterDTO.cs
+++ API/DTOs/MatchesFilterDTO.cs
@@ -4,27 +4,71 @@ using Database.Enums.Verification;
 
 namespace API.DTOs;
 
+/// <summary>
+/// Filtering options for querying <see cref="Database.Entities.Match"/>es
+/// used by <see cref="Controllers.MatchesController.ListAsync"/>
+/// </summary>
 public class MatchesFilterDTO
 {
+    /// <summary>
+    /// Filters results for <see cref="Database.Entities.Match"/>es with a
+    /// matching <see cref="Database.Enums.Ruleset"/>
+    /// </summary>
     public Ruleset? Ruleset { get; set; }
 
+    /// <summary>
+    /// Filters results for <see cref="Database.Entities.Match"/>es with a partially
+    /// matching <see cref="Database.Entities.Match.Name"/>
+    /// </summary>
     public string? Name { get; set; }
 
+    /// <summary>
+    /// Filters results for <see cref="Database.Entities.Match"/>es with a
+    /// <see cref="Database.Entities.Match.StartTime"/> greater than this value
+    /// </summary>
     public DateTime? DateMin { get; set; }
 
+    /// <summary>
+    /// Filters results for <see cref="Database.Entities.Match"/>es with an
+    /// <see cref="Database.Entities.Match.EndTime"/> less than this value
+    /// </summary>
     public DateTime? DateMax { get; set; }
 
+    /// <summary>
+    /// Filters results for <see cref="Database.Entities.Match"/>es with a
+    /// matching <see cref="Database.Enums.Verification.VerificationStatus"/>
+    /// </summary>
     public VerificationStatus? VerificationStatus { get; set; }
 
+    /// <summary>
+    /// Filters results for <see cref="Database.Entities.Match"/>es with a matching <see cref="MatchRejectionReason"/>
+    /// </summary>
     public MatchRejectionReason? RejectionReason { get; set; }
 
+    /// <summary>
+    /// Filters results for <see cref="Database.Entities.Match"/>es with a matching <see cref="MatchProcessingStatus"/>
+    /// </summary>
     public MatchProcessingStatus? ProcessingStatus { get; set; }
 
+    /// <summary>
+    /// Filters results for <see cref="Database.Entities.Match"/>es where the id of the
+    /// <see cref="Database.Entities.User"/> that submitted it matches this value
+    /// </summary>
     public int? SubmittedBy { get; set; }
 
+    /// <summary>
+    /// Filters results for <see cref="Database.Entities.Match"/>es where the id of the
+    /// <see cref="Database.Entities.User"/> that verified it matches this value
+    /// </summary>
     public int? VerifiedBy { get; set; }
 
+    /// <summary>
+    /// Controls the manner in which results are sorted
+    /// </summary>
     public MatchesQuerySortType? Sort { get; set; } = MatchesQuerySortType.Id;
 
+    /// <summary>
+    /// Denotes whether to sort results in ascending or descending order
+    /// </summary>
     public bool? SortDescending { get; set; }
 }
--- Database/Enums/Queries/MatchesQuerySortType.cs
+++ Database/Enums/Queries/MatchesQuerySortType.cs
@@ -1,9 +1,27 @@
 namespace Database.Enums.Queries;
 
+/// <summary>
+/// Denotes which property a query for <see cref="Database.Entities.Matches"/> will be sorted by
+/// </summary>
 public enum MatchesQuerySortType
 {
+    /// <summary>
+    /// Sort by primary key
+    /// </summary>
     Id,
+
+    /// <summary>
+    /// Sort by osu! id
+    /// </summary>
     OsuId,
+
+    /// <summary>
+    /// Sort by start start time
+    /// </summary>
     StartTime,
+
+    /// <summary>
+    /// Sort by end time
+    /// </summary>
     EndTime
 }
