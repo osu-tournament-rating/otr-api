b521447c446fa9c9c5515c1c31f14297bb0fbc7c
Placni <pat.lacni@yahoo.com>
2024-09-01 12:28:50 -0500

    API documentation for new DTOs

--- API/DTOs/PlayerRatingBaseDTO.cs
+++ API/DTOs/PlayerRatingBaseDTO.cs
@@ -3,23 +3,47 @@ using Database.Enums;
 namespace API.DTOs;
 
 /// <summary>
-/// TBD
+/// Describes tournament rating based information for a player in a ruleset that are current and not time specific
 /// </summary>
 public class PlayerRatingBaseDTO
 {
+    /// <summary>
+    /// Ruleset
+    /// </summary>
     public Ruleset Ruleset { get; set; }
 
+    /// <summary>
+    /// Rating
+    /// </summary>
     public double Rating { get; set; }
 
+    /// <summary>
+    /// Rating volatility
+    /// </summary>
     public double Volatility { get; set; }
 
+    /// <summary>
+    /// Global rating percentile
+    /// </summary>
     public double Percentile { get; set; }
 
+    /// <summary>
+    /// Global rank
+    /// </summary>
     public int GlobalRank { get; set; }
 
+    /// <summary>
+    /// Country rank
+    /// </summary>
     public int CountryRank { get; set; }
 
+    /// <summary>
+    /// Player id
+    /// </summary>
     public int PlayerId { get; set; }
 
+    /// <summary>
+    /// A collection of adjustments that describe the changes resulting in the final rating
+    /// </summary>
     public ICollection<RatingAdjustmentDTO> Adjustments { get; set; } = new List<RatingAdjustmentDTO>();
 }
--- API/DTOs/RatingAdjustmentDTO.cs
+++ API/DTOs/RatingAdjustmentDTO.cs
@@ -3,21 +3,52 @@ using Database.Enums;
 namespace API.DTOs;
 
 /// <summary>
-/// TBD
+/// Describes a single change to a PlayerRating
 /// </summary>
 public class RatingAdjustmentDTO
 {
+    /// <summary>
+    /// The type of event that caused the adjustment
+    /// </summary>
     public RatingAdjustmentType AdjustmentType { get; init; }
 
+    /// <summary>
+    /// Timestamp of when the adjustment was applied
+    /// </summary>
     public DateTime Timestamp { get; init; }
 
+    /// <summary>
+    /// Rating before the adjustment
+    /// </summary>
     public double RatingBefore { get; init; }
 
+    /// <summary>
+    /// Rating after the adjustment
+    /// </summary>
     public double RatingAfter { get; init; }
 
+    /// <summary>
+    /// Total change in rating
+    /// </summary>
+    public double RatingDelta { get; init; }
+
+    /// <summary>
+    /// Rating volatility before the adjustment
+    /// </summary>
     public double VolatilityBefore { get; init; }
 
+    /// <summary>
+    /// Rating volatility after the adjustment
+    /// </summary>
     public double VolatilityAfter { get; init; }
 
+    /// <summary>
+    /// Total change in rating volatility
+    /// </summary>
+    public double VolatilityDelta { get; init; }
+
+    /// <summary>
+    /// Id of the match the adjustment was created for if available
+    /// </summary>
     public int? MatchId { get; init; }
 }
