07416d1bca8334a32dfaef9e9c5b12a8c1f07949
Placni <pat.lacni@yahoo.com>
2024-09-02 10:09:37 -0500

    Database migration for RatingAdjustment modifications

--- Database/Migrations/20240902150824_RatingAdjustment_RemoveRankFields.Designer.cs
+++ Database/Migrations/20240902150824_RatingAdjustment_RemoveRankFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Database.Migrations
 {
     [DbContext(typeof(OtrContext))]
-    partial class OtrContextModelSnapshot : ModelSnapshot
+    [Migration("20240902150824_RatingAdjustment_RemoveRankFields")]
+    partial class RatingAdjustment_RemoveRankFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
