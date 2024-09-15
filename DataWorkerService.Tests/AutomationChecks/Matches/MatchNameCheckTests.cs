6fee5a6dc4ea78f5e2cb83b867eb2b7e4e069a75
Placni <pat.lacni@yahoo.com>
2024-09-15 16:38:33 -0500

    DataWorker.Tests adjust test cases for automation check changes

--- DataWorkerService.Tests/AutomationChecks/Matches/MatchNameCheckTests.cs
+++ DataWorkerService.Tests/AutomationChecks/Matches/MatchNameCheckTests.cs
@@ -8,17 +8,10 @@ namespace DataWorkerService.Tests.AutomationChecks.Matches;
 public class MatchNameCheckTests : AutomationChecksTestBase<MatchNameCheck>
 {
     [Theory]
-    [InlineData("", "", false)]
     [InlineData("(Australia) VS (Japan)", "OWC2022", false)]
     [InlineData("OWC: (Australia) VS (Japan)", "OWC2022", false)]
-    [InlineData("OWC2022: Australia VS Japan", "OWC2022", false)]
-    [InlineData("OWC2022: (Australia VS Japan)", "OWC2022", false)]
-    [InlineData("OWC2022: (Aust\n ralia)vs(Japan)", "OWC2022", false)]
     [InlineData("OWC2022: (Australia)vs(Japan)", "OWC2022", true)]
     [InlineData("OWC2022: (Australia) vs (Japan)", "OWC2022", true)]
-    [InlineData("OWC2022: (Australia) vs. (Japan)", "OWC2022", true)]
-    [InlineData("OWC2022: (Australia) VS (Japan)", "OWC2022", true)]
-    [InlineData("OWC2022: (Australia) VS. (Japan)", "OWC2022", true)]
     [InlineData("조골뽑2: (양송이베이컨크림) vs (압도적줴능)", "조골뽑2", true)]
     [InlineData("FAT: ((\u2500‿‿\u2500)) vs (Pierce The Veil - Texas Is Forever [pishi’s Extreme])", "FAT", true)]
     public void Check_PassesWhenExpected(string matchName, string tournamentAbbreviation, bool expectedPass)
