a233dd1045a4c669cb9d13c79c3b3b7986aba245
hburn7 <hburnett777@gmail.com>
2024-09-22 20:56:01 -0400

    Create migration

---
+++ Database/Migrations/20240923005516_PlayerAdminNotes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlayerAdminNotes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "player_admin_notes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    note = table.Column<string>(type: "text", nullable: false),
+                    ref_id = table.Column<int>(type: "integer", nullable: false),
+                    admin_user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_player_admin_notes", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_player_admin_notes_players_ref_id",
+                        column: x => x.ref_id,
+                        principalTable: "players",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_player_admin_notes_ref_id",
+                table: "player_admin_notes",
+                column: "ref_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "player_admin_notes");
+        }
+    }
+}
