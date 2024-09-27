8169cbf03d349cb8e7a75f3524c21f1fddb800fa
Placni <pat.lacni@yahoo.com>
2024-09-26 20:47:53 -0500

    API add service layer for admin note CRUD actions

---
+++ API/DTOs/AdminNoteDTO.cs
@@ -0,0 +1,42 @@
+namespace API.DTOs;
+
+/// <summary>
+/// Represents a note for an entity created by an admin
+/// </summary>
+public class AdminNoteDTO
+{
+    /// <summary>
+    /// Id
+    /// </summary>
+    public int Id { get; init; }
+
+    /// <summary>
+    /// Timestamp of creation
+    /// </summary>
+    public DateTime Created { get; init; }
+
+    /// <summary>
+    /// Timestamp of the last update if available
+    /// </summary>
+    public DateTime? Updated { get; init; }
+
+    /// <summary>
+    /// Id of the parent entity
+    /// </summary>
+    public int ReferenceId { get; init; }
+
+    /// <summary>
+    /// Id of the admin user that created the note
+    /// </summary>
+    public int AdminUserId { get; set; }
+
+    /// <summary>
+    /// Username of the admin user that created the note
+    /// </summary>
+    public string? AdminUsername { get; init; }
+
+    /// <summary>
+    /// Content of the note
+    /// </summary>
+    public string Note { get; init; } = string.Empty;
+}
