d9fc50e416fdea625389865bfd8f38ab318c0290
Placni <pat.lacni@yahoo.com>
2024-09-26 22:06:57 -0500

    API CRUD endpoints for tournament admin notes

---
+++ API/Controllers/TournamentsController.Admin.cs
@@ -0,0 +1,162 @@
+using API.DTOs;
+using API.Utilities;
+using API.Utilities.Extensions;
+using Database.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public partial class TournamentsController
+{
+    /// <summary>
+    /// Creates an admin note for a tournament
+    /// </summary>
+    /// <param name="id">Tournament id</param>
+    /// <response code="401">If the requester is not properly authorized</response>
+    /// <response code="404">If a tournament matching the given id does not exist</response>
+    /// <response code="400">If the authorized user does not exist</response>
+    /// <response code="200">Returns the created admin note</response>
+    [HttpPost("{id:int}/notes")]
+    [Authorize(Roles = $"{OtrClaims.Admin}")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType<AdminNoteDTO>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> CreateAdminNoteAsync(int id, [FromBody] string note)
+    {
+        var adminUserId = User.AuthorizedIdentity();
+        if (!adminUserId.HasValue)
+        {
+            return Unauthorized();
+        }
+
+        if (!await tournamentsService.ExistsAsync(id))
+        {
+            return NotFound();
+        }
+
+        AdminNoteDTO? result = await adminNoteService.CreateAsync<TournamentAdminNote>(id, adminUserId.Value, note);
+        return result is not null
+            ? Ok(result)
+            : BadRequest();
+    }
+
+    /// <summary>
+    /// List all admin notes from a tournament
+    /// </summary>
+    /// <param name="id">Tournament id</param>
+    /// <response code="404">If a tournament matching the given id does not exist</response>
+    /// <response code="200">Returns all admin notes from a tournament</response>
+    [HttpGet("{id:int}/notes")]
+    [Authorize(Roles = $"{OtrClaims.Admin}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType<IEnumerable<AdminNoteDTO>>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> ListAdminNotesAsync(int id)
+    {
+        if (!await tournamentsService.ExistsAsync(id))
+        {
+            return NotFound();
+        }
+
+        return Ok(await adminNoteService.GetAsync<TournamentAdminNote>(id));
+    }
+
+    /// <summary>
+    /// Updates an admin note for a tournament
+    /// </summary>
+    /// <param name="id">Tournament id</param>
+    /// <param name="noteId">Admin note id</param>
+    /// <response code="401">If the requester is not properly authorized</response>
+    /// <response code="404">
+    /// If a tournament matching the given id does not exist.
+    /// If an admin note matching the given noteId does not exist
+    /// </response>
+    /// <response code="403">If the requester did not create the admin note</response>
+    /// <response code="200">Returns the updated admin note</response>
+    [HttpPatch("{id:int}/notes/{noteId:int}")]
+    [Authorize(Roles = $"{OtrClaims.Admin}")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType<AdminNoteDTO>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> UpdateAdminNoteAsync(int id, int noteId, [FromBody] string note)
+    {
+        var adminUserId = User.AuthorizedIdentity();
+        if (!adminUserId.HasValue)
+        {
+            return Unauthorized();
+        }
+
+        if (!await tournamentsService.ExistsAsync(id))
+        {
+            return NotFound();
+        }
+
+        AdminNoteDTO? existingNote = await adminNoteService.GetAsync<TournamentAdminNote>(noteId);
+        if (existingNote is null)
+        {
+            return NotFound();
+        }
+
+        if (adminUserId != existingNote.AdminUserId)
+        {
+            return Forbid();
+        }
+
+        existingNote.Note = note;
+        AdminNoteDTO? result = await adminNoteService.UpdateAsync<TournamentAdminNote>(existingNote);
+
+        return result is not null
+            ? Ok(result)
+            : NotFound();
+    }
+
+    /// <summary>
+    /// Deletes an admin note for a tournament
+    /// </summary>
+    /// <param name="id">Tournament id</param>
+    /// <param name="noteId">Admin note id</param>
+    /// <response code="401">If the requester is not properly authorized</response>
+    /// <response code="404">
+    /// If a tournament matching the given id does not exist.
+    /// If an admin note matching the given noteId does not exist
+    /// </response>
+    /// <response code="403">If the requester did not create the admin note</response>
+    /// <response code="200">Returns the updated admin note</response>
+    [HttpPatch("{id:int}/notes/{noteId:int}")]
+    [Authorize(Roles = $"{OtrClaims.Admin}")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType<AdminNoteDTO>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> DeleteAdminNoteAsync(int id, int noteId)
+    {
+        var adminUserId = User.AuthorizedIdentity();
+        if (!adminUserId.HasValue)
+        {
+            return Unauthorized();
+        }
+
+        if (!await tournamentsService.ExistsAsync(id))
+        {
+            return NotFound();
+        }
+
+        AdminNoteDTO? existingNote = await adminNoteService.GetAsync<TournamentAdminNote>(noteId);
+        if (existingNote is null)
+        {
+            return NotFound();
+        }
+
+        if (adminUserId != existingNote.AdminUserId)
+        {
+            return Forbid();
+        }
+
+        var result = await adminNoteService.DeleteAsync<TournamentAdminNote>(noteId);
+        return result
+            ? Ok()
+            : NotFound();
+    }
+}
