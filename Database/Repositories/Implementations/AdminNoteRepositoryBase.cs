abd103e0fe1c93c8d3c9894cd6c10e9a90713df6
Placni <pat.lacni@yahoo.com>
2024-09-26 19:38:28 -0500

    Database CRUD actions for admin note repository

--- Database/Repositories/Implementations/AdminNoteRepositoryBase.cs
+++ Database/Repositories/Implementations/AdminNoteRepositoryBase.cs
@@ -1,11 +1,45 @@
 using Database.Entities;
 using Database.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Database.Repositories.Implementations;
 
-public abstract class AdminNoteRepositoryBase<TAdminNoteEntity>(OtrContext context)
-    : RepositoryBase<TAdminNoteEntity>(context), IAdminNoteRepository<TAdminNoteEntity>
-    where TAdminNoteEntity : AdminNoteEntityBase
+public class AdminNoteRepository(OtrContext context) : IAdminNoteRepository
 {
-    public abstract Task<ICollection<TAdminNoteEntity>> GetAdminNotesAsync(int referenceId);
+    public async Task CreateAsync<TAdminNote>(TAdminNote entity) where TAdminNote : AdminNoteEntityBase
+    {
+        context.Set<TAdminNote>().Add(entity);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task<TAdminNote?> GetAsync<TAdminNote>(int id) where TAdminNote : AdminNoteEntityBase =>
+        await context.Set<TAdminNote>().FirstOrDefaultAsync(an => an.Id == id);
+
+    public async Task<IEnumerable<TAdminNote>> ListAsync<TAdminNote>(int referenceId) where TAdminNote : AdminNoteEntityBase =>
+        await context.Set<TAdminNote>()
+            .Where(an => an.ReferenceId == referenceId)
+            .Include(an => an.AdminUser.Player)
+            .AsNoTracking()
+            .AsSingleQuery()
+            .ToListAsync();
+
+    public async Task<TAdminNote> UpdateAsync<TAdminNote>(TAdminNote entity) where TAdminNote : AdminNoteEntityBase
+    {
+        EntityEntry<TAdminNote> entry = context.Set<TAdminNote>().Entry(entity);
+
+        if (entry.State is EntityState.Unchanged)
+        {
+            return entity;
+        }
+
+        await context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task DeleteAsync<TAdminNote>(TAdminNote entity) where TAdminNote : AdminNoteEntityBase
+    {
+        context.Set<TAdminNote>().Remove(entity);
+        await context.SaveChangesAsync();
+    }
 }
