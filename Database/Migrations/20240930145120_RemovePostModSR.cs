aba5e29dab03190ea90b7eb3317bb19e162d6d72
bugg86 <jackfink68@yahoo.com>
2024-09-30 11:23:54 -0400

    updated migration with drop column command

--- Database/Migrations/20240930145120_RemovePostModSR.cs
+++ Database/Migrations/20240930145120_RemovePostModSR.cs
@@ -10,7 +10,7 @@ namespace Database.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn("post_mod_sr", "games");
         }
 
         /// <inheritdoc />
