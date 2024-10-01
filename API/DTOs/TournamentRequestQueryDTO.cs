6029f911dcaa7469b7b97e814856ca6b168bd3e8
hburn7 <hburnett777@gmail.com>
2024-09-30 23:53:03 -0400

    Use data annotations

--- API/DTOs/TournamentRequestQueryDTO.cs
+++ API/DTOs/TournamentRequestQueryDTO.cs
@@ -1,24 +1,35 @@
+using System.ComponentModel.DataAnnotations;
 using Database.Enums;
+using static Database.Enums.Ruleset;
 
 namespace API.DTOs;
 
-// Can be expanded to support filters like leaderboards if needed
+/// <summary>
+/// Enables pagination and filtering of tournament requests
+/// </summary>
 public class TournamentRequestQueryDTO
 {
     /// <summary>
-    /// The page
+    /// The page number
     /// </summary>
-    public int Page { get; set; }
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+    public int Page { get; set; } = 1;
+
     /// <summary>
     /// The size of the page
     /// </summary>
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
     public int PageSize { get; set; } = 20;
+
     /// <summary>
     /// Whether the tournaments must be verified
     /// </summary>
     public bool Verified { get; set; } = true;
+
     /// <summary>
     /// An optional ruleset to filter by
     /// </summary>
-    public Ruleset? Ruleset { get; set; }
+    public Ruleset? Ruleset { get; set; } = Osu;
 }
