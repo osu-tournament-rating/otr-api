dd92fd6aaeed9504169a72ac549c8d2d27b758e3
hburn7 <hburnett777@gmail.com>
2024-10-13 11:03:25 -0400

    Create TournamentCompactDTO

---
+++ API/DTOs/TournamentCompactDTO.cs
@@ -0,0 +1,77 @@
+using System.Diagnostics.CodeAnalysis;
+using Database.Enums;
+using Database.Enums.Verification;
+
+namespace API.DTOs;
+
+public class TournamentCompactDTO
+{
+    /// <summary>
+    /// The tournament id
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// The name of the tournament
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Acronym / shortened name of the tournament
+    /// <example>For osu! World Cup 2023, this value would be "OWC23"</example>
+    /// </summary>
+    public string Abbreviation { get; set; } = null!;
+
+    /// <summary>
+    /// The osu! forum post advertising this tournament
+    /// </summary>
+    public string ForumUrl { get; set; } = null!;
+
+    /// <summary>
+    /// Lowest rank a player can be to participate in the tournament
+    /// </summary>
+    /// <example>For a 10,000-50,000 tournament, this value would be 10,000</example>
+    public int RankRangeLowerBound { get; set; }
+
+    /// <summary>
+    /// osu! ruleset
+    /// </summary>
+    public Ruleset Ruleset { get; set; }
+
+    /// <summary>
+    /// Expected in-match team size
+    /// </summary>
+    /// <example>For a 2v2 team size 4 tournament, this value should be 2</example>
+    public int LobbySize { get; set; }
+
+    /// <summary>
+    /// The current state of verification
+    /// </summary>
+    public VerificationStatus VerificationStatus { get; set; }
+
+    /// <summary>
+    /// The current state of processing
+    /// </summary>
+    public TournamentProcessingStatus ProcessingStatus { get; set; }
+
+    /// <summary>
+    /// The tournament rejection reason
+    /// </summary>
+    public TournamentRejectionReason RejectionReason { get; set; }
+
+    /// <summary>
+    /// The timestamp of submission for the tournament
+    /// </summary>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public DateTime Created { get; set; }
+
+    /// <summary>
+    /// The start date of the first match played in the tournament
+    /// </summary>
+    public DateTime StartTime { get; set; }
+
+    /// <summary>
+    /// The end date of the last match played in the tournament
+    /// </summary>
+    public DateTime EndTime { get; set; }
+}
