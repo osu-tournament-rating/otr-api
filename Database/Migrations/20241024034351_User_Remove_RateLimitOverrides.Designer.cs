dec16d236b1606a9a352ed6fa6e37108c06fb2ba
Placni <pat.lacni@yahoo.com>
2024-10-23 22:44:27 -0500

    Database migration for user rlo removal

--- Database/Migrations/20241024034351_User_Remove_RateLimitOverrides.Designer.cs
+++ Database/Migrations/20241024034351_User_Remove_RateLimitOverrides.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Database.Migrations
 {
     [DbContext(typeof(OtrContext))]
-    partial class OtrContextModelSnapshot : ModelSnapshot
+    [Migration("20241024034351_User_Remove_RateLimitOverrides")]
+    partial class User_Remove_RateLimitOverrides
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
