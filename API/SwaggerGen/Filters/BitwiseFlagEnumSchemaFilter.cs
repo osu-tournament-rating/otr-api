a3fe88494907c7454e82ab8da42676bd13a64f9c
Placni <pat.lacni@yahoo.com>
2024-11-07 20:01:33 -0600

    API code quality for swagger-gen stuff

--- API/SwaggerGen/Filters/BitwiseFlagEnumSchemaFilter.cs
+++ API/SwaggerGen/Filters/BitwiseFlagEnumSchemaFilter.cs
@@ -1,13 +1,15 @@
 using System.Reflection;
+using JetBrains.Annotations;
 using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
-namespace API.SwaggerGen;
+namespace API.SwaggerGen.Filters;
 
 /// <summary>
 /// Appends a custom attribute to the schemas generated for enums that are bitwise flags
 /// </summary>
+[UsedImplicitly]
 public class BitwiseFlagEnumSchemaFilter : ISchemaFilter
 {
     public void Apply(OpenApiSchema schema, SchemaFilterContext context)
