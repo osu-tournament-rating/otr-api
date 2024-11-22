f474ef66d057283e27ec4bed9477e9151b7b86e3
Placni <pat.lacni@yahoo.com>
2024-11-22 06:16:29 -0600

    code quality for logging and authorization

--- API/Authorization/Handlers/AccessUserResourcesAuthorizationHandler.cs
+++ API/Authorization/Handlers/AccessUserResourcesAuthorizationHandler.cs
@@ -5,35 +5,37 @@ using Microsoft.AspNetCore.Authorization;
 namespace API.Authorization.Handlers;
 
 /// <summary>
-/// Handles authorization for the policy <see cref="AuthorizationPolicies.AccessUserResources"/>
+/// Determines whether authorization is allowed for a <see cref="AccessUserResourcesAuthorizationRequirement"/>
 /// </summary>
-public class AccessUserResourcesAuthorizationHandler : AuthorizationHandler<AccessUserResourcesRequirement>
+public class AccessUserResourcesAuthorizationHandler(
+    IHttpContextAccessor httpContextAccessor
+) : AuthorizationHandler<AccessUserResourcesAuthorizationRequirement>
 {
-    /// <inheritdoc/>
+    private readonly HttpContext? _httpContext = httpContextAccessor.HttpContext;
+
     protected override Task HandleRequirementAsync(
         AuthorizationHandlerContext context,
-        AccessUserResourcesRequirement requirement
+        AccessUserResourcesAuthorizationRequirement requirement
     )
     {
-        if (context.Resource is not HttpContext httpContext)
+        if (_httpContext is null || context.User.Identity is not { IsAuthenticated: true })
         {
             context.Fail();
             return Task.CompletedTask;
         }
 
         // Reject if the request does not pass "id" in route
-        if (!int.TryParse(httpContext.Request.RouteValues["id"]?.ToString(), out var routeId))
+        if (int.TryParse(_httpContext.Request.RouteValues["id"]?.ToString(), out var routeId))
         {
-            context.Fail();
-            return Task.CompletedTask;
+            // Allow requests that are privileged or have matching user id and target route id
+            if (context.User.IsAdmin() || context.User.GetSubjectId() == routeId)
+            {
+                context.Succeed(requirement);
+                return Task.CompletedTask;
+            }
         }
 
-        // Allow requests that are privileged or have matching user id and target route id
-        if (context.User.IsAdmin() || (requirement.AllowSelf && context.User.GetSubjectId() == routeId))
-        {
-            context.Succeed(requirement);
-        }
-
+        context.Fail();
         return Task.CompletedTask;
     }
 }
