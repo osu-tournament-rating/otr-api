1e191f06c7380188eb6c5ea93aee631fda74e013
Placni <pat.lacni@yahoo.com>
2024-11-21 20:54:57 -0600

    API fix unhandled exceptions in the whitelist middleware

--- API/Middlewares/WhitelistEnforcementMiddleware.cs
+++ API/Middlewares/WhitelistEnforcementMiddleware.cs
@@ -1,3 +1,4 @@
+using API.Authorization;
 using API.Utilities.Extensions;
 using Microsoft.AspNetCore.Authorization;
 
@@ -6,12 +7,22 @@ namespace API.Middlewares;
 /// <summary>
 /// Middleware that enforces all requesting ClaimsPrinciples contain the whitelist claim
 /// </summary>
+/// <remarks>
+/// This middleware should always be registered AFTER the Authentication middleware
+/// </remarks>
 public class WhitelistEnforcementMiddleware(RequestDelegate next, ILogger<WhitelistEnforcementMiddleware> logger)
 {
     public async Task Invoke(HttpContext context)
     {
         // Check for [AllowAnonymous]
         if (context.GetEndpoint()?.Metadata.GetMetadata<IAllowAnonymous>() is not null)
+        {
+            await next(context);
+            return;
+        }
+
+        // If authentication wasn't successful the request will be rejected anyways
+        if (context.User.Identity is { IsAuthenticated: false })
         {
             await next(context);
             return;
@@ -32,7 +43,23 @@ public class WhitelistEnforcementMiddleware(RequestDelegate next, ILogger<Whitel
         }
 #endif
 
-        logger.LogInformation("Rejecting client with identity {id} for whitelist violation", context.User.GetSubjectId());
+        string? ident = null;
+        try
+        {
+            ident = context.User.GetSubjectId().ToString();
+        }
+        catch
+        {
+            // ignored, we can expect null ids here
+        }
+        ident ??= "unknown";
+
+        logger.LogInformation(
+            "Rejecting {Requester} with identity {Ident} for whitelist violation",
+            (context.User.IsClient() ? OtrClaims.Roles.Client : OtrClaims.Roles.User).ToLower(),
+            ident
+        );
+
         context.Response.StatusCode = StatusCodes.Status403Forbidden;
     }
 }
