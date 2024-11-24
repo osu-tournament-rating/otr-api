1003b4ac9a1e3937136b55a128ec4ea84eaf047f
hburn7 <hburnett777@gmail.com>
2024-11-24 11:42:06 -0500

    endpoint documentation

--- API/Controllers/DiagnosticsController.cs
+++ API/Controllers/DiagnosticsController.cs
@@ -9,6 +9,10 @@ namespace API.Controllers;
 [Route("api/v{version:apiVersion}/[controller]")]
 public class DiagnosticsController : Controller
 {
+    /// <summary>
+    /// Allows clients to determine if the server is running  
+    /// </summary>
+    /// <response code="200>The server is running</response> 
     [HttpGet("ping")]
     [AllowAnonymous]
     [ProducesResponseType(StatusCodes.Status200OK)]
