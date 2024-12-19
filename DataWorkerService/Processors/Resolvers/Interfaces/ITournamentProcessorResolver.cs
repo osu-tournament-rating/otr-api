8dddfa48c54d13acb15a7cf9067a384b92e6f03d
hburn7 <hburnett777@gmail.com>
2024-12-19 15:24:03 +0900

    Document GetNextProcessor

--- DataWorkerService/Processors/Resolvers/Interfaces/ITournamentProcessorResolver.cs
+++ DataWorkerService/Processors/Resolvers/Interfaces/ITournamentProcessorResolver.cs
@@ -16,5 +16,14 @@ public interface ITournamentProcessorResolver : IProcessorResolver<Tournament>
     /// </summary>
     IProcessor<Tournament> GetStatsProcessor();
 
+    /// <summary>
+    ///     The processor which a tournament with the given <see cref="processingStatus" /> should
+    ///     be executed against
+    /// </summary>
+    /// <param name="processingStatus">A <see cref="Tournament" />'s current ProcessingStatus</param>
+    /// <returns>
+    ///     The processor which is to be executed next for any <see cref="Tournament" />
+    ///     which has the given <see cref="processingStatus" />
+    /// </returns>
     IProcessor<Tournament> GetNextProcessor(TournamentProcessingStatus processingStatus);
 }
