a38dd3965dc28a24a736568cdc6efa9d5e449ffd
hburn7 <hburnett777@gmail.com>
2025-01-05 20:00:47 -0500

    Add migration

---
+++ Database/Migrations/20250106005917_MatchWinRecord_DeleteBehavior_Cascade.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class MatchWinRecord_DeleteBehavior_Cascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_match_win_records_matches_match_id",
+                table: "match_win_records");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_match_win_records_matches_match_id",
+                table: "match_win_records",
+                column: "match_id",
+                principalTable: "matches",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_match_win_records_matches_match_id",
+                table: "match_win_records");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_match_win_records_matches_match_id",
+                table: "match_win_records",
+                column: "match_id",
+                principalTable: "matches",
+                principalColumn: "id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
