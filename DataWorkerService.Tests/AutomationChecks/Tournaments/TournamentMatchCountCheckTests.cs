226b330e10df45b6306ac3bec4bb2531ec3cc196
majorro <majorrowk@gmail.com>
2025-03-10 20:53:49 +0300

    Inline CheckTournament in TournamentMatchCountCheckTests

--- DataWorkerService.Tests/AutomationChecks/Tournaments/TournamentMatchCountCheckTests.cs
+++ DataWorkerService.Tests/AutomationChecks/Tournaments/TournamentMatchCountCheckTests.cs
@@ -22,107 +22,119 @@ public class TournamentMatchCountCheckTests : AutomationChecksTestBase<Tournamen
         bool expectedPass
     )
     {
-        CheckTournament(
-            tournament =>
-            {
-                if (verifiedMatchCount >= 1)
-                {
-                    foreach (var _ in Enumerable.Range(1, verifiedMatchCount))
-                    {
-                        SeededMatch.Generate(
-                            verificationStatus: VerificationStatus.Verified,
-                            rejectionReason: MatchRejectionReason.None,
-                            processingStatus: MatchProcessingStatus.Done,
-                            warningFlags: MatchWarningFlags.None,
-                            tournament: tournament);
-                    }
-                }
-
-                if (totalMatchCount - verifiedMatchCount >= 1)
-                {
-                    foreach (var _ in Enumerable.Range(1, totalMatchCount - verifiedMatchCount))
-                    {
-                        SeededMatch.Generate(
-                            verificationStatus: VerificationStatus.Rejected,
-                            tournament: tournament,
-                            rejectionReason: MatchRejectionReason.NoValidGames);
-                    }
-                }
-            },
-            expectedPass: expectedPass
-        );
-    }
-
-    [Fact]
-    public void Check_GivenNoMatches_FailsWith_NoVerifiedMatches() =>
-        CheckTournament(
-            _ => { },
-            expectedPass: false,
-            expectedRejectionReason: TournamentRejectionReason.NoVerifiedMatches
-        );
-
-    [Fact]
-    public void Check_GivenNoVerifiedMatches_FailsWith_NoVerifiedMatches() =>
-        CheckTournament(
-            tournament =>
-            {
-                SeededMatch.Generate(verificationStatus: VerificationStatus.PreRejected, tournament: tournament);
-                SeededMatch.Generate(verificationStatus: VerificationStatus.Rejected, tournament: tournament);
-            },
-            expectedPass: false,
-            expectedRejectionReason: TournamentRejectionReason.NoVerifiedMatches
-        );
-
-    [Fact]
-    public void Check_GivenVerifiedAndEmptyMatches_Passes() =>
-        CheckTournament(
-            tournament =>
-            {
-                SeededMatch.Generate(verificationStatus: VerificationStatus.Verified, tournament: tournament);
-                SeededMatch.Generate(verificationStatus: VerificationStatus.Rejected, rejectionReason: MatchRejectionReason.NoGames, tournament: tournament);
-            },
-            expectedPass: true
-        );
-
-    [Fact]
-    public void Check_GivenVerifiedAndRejectedAndEmptyMatches_FailsWith_NotEnoughVerifiedMatches() =>
-        CheckTournament(
-            tournament =>
-            {
-                SeededMatch.Generate(verificationStatus: VerificationStatus.Verified, tournament: tournament);
-                SeededMatch.Generate(verificationStatus: VerificationStatus.Rejected, rejectionReason: MatchRejectionReason.NoGames, tournament: tournament);
-                SeededMatch.Generate(verificationStatus: VerificationStatus.Rejected, rejectionReason: MatchRejectionReason.NoValidGames, tournament: tournament);
-            },
-            expectedPass: false,
-            expectedRejectionReason: TournamentRejectionReason.NotEnoughVerifiedMatches
-        );
-
-    [Fact]
-    public void Check_GivenEmptyMatches_FailsWith_NoVerifiedMatches() =>
-        CheckTournament(
-            tournament =>
-            {
-                SeededMatch.Generate(verificationStatus: VerificationStatus.Rejected, rejectionReason: MatchRejectionReason.NoGames, tournament: tournament);
-                SeededMatch.Generate(verificationStatus: VerificationStatus.Rejected, rejectionReason: MatchRejectionReason.NoGames, tournament: tournament);
-            },
-            expectedPass: false,
-            expectedRejectionReason: TournamentRejectionReason.NoVerifiedMatches
-        );
-
-    private static void CheckTournament(Action<Tournament> setup, bool expectedPass, TournamentRejectionReason? expectedRejectionReason = null)
-    {
         // Arrange
         Tournament tournament = SeededTournament.Generate(rejectionReason: TournamentRejectionReason.None);
-        setup(tournament);
+
+        if (verifiedMatchCount >= 1)
+        {
+            foreach (var _ in Enumerable.Range(1, verifiedMatchCount))
+            {
+                SeededMatch.Generate(
+                    verificationStatus: VerificationStatus.Verified,
+                    rejectionReason: MatchRejectionReason.None,
+                    processingStatus: MatchProcessingStatus.Done,
+                    warningFlags: MatchWarningFlags.None,
+                    tournament: tournament);
+            }
+        }
+
+        if (totalMatchCount - verifiedMatchCount >= 1)
+        {
+            foreach (var _ in Enumerable.Range(1, totalMatchCount - verifiedMatchCount))
+            {
+                SeededMatch.Generate(
+                    verificationStatus: VerificationStatus.Rejected,
+                    tournament: tournament,
+                    rejectionReason: MatchRejectionReason.NoValidGames);
+            }
+        }
 
         // Act
         var actualPass = AutomationCheck.Check(tournament);
 
         // Assert
         Assert.Equal(expectedPass, actualPass);
-        if (expectedRejectionReason.HasValue)
-        {
-            Assert.Equal(expectedRejectionReason, tournament.RejectionReason);
-        }
+    }
+
+    [Fact]
+    public void Check_GivenNoMatches_FailsWith_NoVerifiedMatches()
+    {
+        // Arrange
+        Tournament tournament = SeededTournament.Generate(rejectionReason: TournamentRejectionReason.None);
+
+        // Act
+        var actualPass = AutomationCheck.Check(tournament);
+
+        // Assert
+        Assert.False(actualPass);
+        Assert.Equal(TournamentRejectionReason.NoVerifiedMatches, tournament.RejectionReason);
+    }
+
+    [Fact]
+    public void Check_GivenNoVerifiedMatches_FailsWith_NoVerifiedMatches()
+    {
+        // Arrange
+        Tournament tournament = SeededTournament.Generate(rejectionReason: TournamentRejectionReason.None);
+
+        SeededMatch.Generate(verificationStatus: VerificationStatus.PreRejected, tournament: tournament);
+        SeededMatch.Generate(verificationStatus: VerificationStatus.Rejected, tournament: tournament);
+
+        // Act
+        var actualPass = AutomationCheck.Check(tournament);
+
+        // Assert
+        Assert.False(actualPass);
+        Assert.Equal(TournamentRejectionReason.NoVerifiedMatches, tournament.RejectionReason);
+    }
+
+    [Fact]
+    public void Check_GivenVerifiedAndEmptyMatches_Passes()
+    {
+        // Arrange
+        Tournament tournament = SeededTournament.Generate(rejectionReason: TournamentRejectionReason.None);
+
+        SeededMatch.Generate(verificationStatus: VerificationStatus.Verified, tournament: tournament);
+        SeededMatch.Generate(verificationStatus: VerificationStatus.Rejected, rejectionReason: MatchRejectionReason.NoGames, tournament: tournament);
+
+        // Act
+        var actualPass = AutomationCheck.Check(tournament);
+
+        // Assert
+        Assert.True(actualPass);
+    }
+
+    [Fact]
+    public void Check_GivenVerifiedAndRejectedAndEmptyMatches_FailsWith_NotEnoughVerifiedMatches()
+    {
+        // Arrange
+        Tournament tournament = SeededTournament.Generate(rejectionReason: TournamentRejectionReason.None);
+
+        SeededMatch.Generate(verificationStatus: VerificationStatus.Verified, tournament: tournament);
+        SeededMatch.Generate(verificationStatus: VerificationStatus.Rejected, rejectionReason: MatchRejectionReason.NoGames, tournament: tournament);
+        SeededMatch.Generate(verificationStatus: VerificationStatus.Rejected, rejectionReason: MatchRejectionReason.NoValidGames, tournament: tournament);
+
+        // Act
+        var actualPass = AutomationCheck.Check(tournament);
+
+        // Assert
+        Assert.False(actualPass);
+        Assert.Equal(TournamentRejectionReason.NotEnoughVerifiedMatches, tournament.RejectionReason);
+    }
+
+    [Fact]
+    public void Check_GivenEmptyMatches_FailsWith_NoVerifiedMatches()
+    {
+        // Arrange
+        Tournament tournament = SeededTournament.Generate(rejectionReason: TournamentRejectionReason.None);
+
+        SeededMatch.Generate(verificationStatus: VerificationStatus.Rejected, rejectionReason: MatchRejectionReason.NoGames, tournament: tournament);
+        SeededMatch.Generate(verificationStatus: VerificationStatus.Rejected, rejectionReason: MatchRejectionReason.NoGames, tournament: tournament);
+
+        // Act
+        var actualPass = AutomationCheck.Check(tournament);
+
+        // Assert
+        Assert.False(actualPass);
+        Assert.Equal(TournamentRejectionReason.NoVerifiedMatches, tournament.RejectionReason);
     }
 }
