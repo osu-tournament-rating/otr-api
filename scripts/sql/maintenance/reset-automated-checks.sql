a849843690b64c937804ecadf5eb765e12e98964
hburn7 <hburnett777@gmail.com>
2025-03-12 20:05:57 -0400

    Update reset automation checks script to not target rejected data

--- scripts/sql/maintenance/reset-automated-checks.sql
+++ scripts/sql/maintenance/reset-automated-checks.sql
@@ -5,22 +5,22 @@ UPDATE tournaments
 SET rejection_reason = 0,
     verification_status = 0,
     processing_status = 2
-WHERE verification_status != 4;
+WHERE verification_status < 3;
 
 UPDATE matches
 SET rejection_reason = 0,
     verification_status = 0,
     processing_status = 1
-WHERE verification_status != 4;
+WHERE verification_status < 3;
 
 UPDATE games
 SET rejection_reason = 0,
     verification_status = 0,
     processing_status = 0
-WHERE verification_status != 4;
+WHERE verification_status < 3;
 
 UPDATE game_scores
 SET rejection_reason = 0,
     verification_status = 0,
     processing_status = 0
-WHERE verification_status != 4;
+WHERE verification_status < 3;
