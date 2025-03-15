0dc08dd94b14ceced8cf9b0a112c7c45d76533fb
myssto <pat.lacni@yahoo.com>
2025-03-15 05:43:49 -0500

    wip admin note controller

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,7 @@ using API.Services.Interfaces;
 using API.SwaggerGen;
 using API.SwaggerGen.Filters;
 using API.Utilities;
+using API.Utilities.AdminNotes;
 using API.Utilities.Extensions;
 using Asp.Versioning;
 using AutoMapper;
@@ -247,6 +248,11 @@ builder.Services.AddSwaggerGen(options =>
     options.OperationFilter<SecurityMetadataOperationFilter>();
     options.OperationFilter<DiscardNestedParametersOperationFilter>();
 
+    options.SchemaFilter<OverrideSchemaFilter<AdminNoteRouteTarget>>((OpenApiSchema schema, SchemaFilterContext ctx) =>
+    {
+
+    });
+
     options.SchemaFilter<EnumMetadataSchemaFilter>();
     options.SchemaFilter<RequireNonNullablePropertiesSchemaFilter>();
 
@@ -276,8 +282,7 @@ builder.Services.AddSwaggerGen(options =>
         }
         else
         {
-            method = $"method_{unknownMethodCount}";
-            unknownMethodCount++;
+            method = $"method_{unknownMethodCount++}";
         }
 
         return $"{controller}_{method}";
