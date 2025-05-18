c20155070b1c0be94eb551dda0d1af5728350cd7
majorro <majorrowk@gmail.com>
2025-05-18 22:19:03 +0300

    remove keys imputing for countsBy tournament stats

--- API/Services/Implementations/TournamentPlatformStatsService.cs
+++ API/Services/Implementations/TournamentPlatformStatsService.cs
@@ -13,16 +13,9 @@ public class TournamentPlatformStatsService(ITournamentsRepository tournamentsRe
     public async Task<TournamentPlatformStatsDTO> GetAsync()
     {
         Dictionary<VerificationStatus, int> countsByStatuses = await tournamentsRepository.GetVerificationStatusStatsAsync();
-        ImputeMissingKeys(countsByStatuses, EnumUtils.MinValue<VerificationStatus>(), EnumUtils.MaxValue<VerificationStatus>());
-
         Dictionary<int, int> countsByYears = await tournamentsRepository.GetYearStatsAsync();
-        ImputeMissingKeys(countsByYears);
-
         Dictionary<Ruleset, int> countsByRulesets = await tournamentsRepository.GetRulesetStatsAsync();
-        ImputeMissingKeys(countsByRulesets, EnumUtils.MinValue<Ruleset>(), EnumUtils.MaxValue<Ruleset>());
-
         Dictionary<int, int> countsByLobbySizes = await tournamentsRepository.GetLobbySizeStatsAsync();
-        ImputeMissingKeys(countsByLobbySizes, LobbySizeConstants.MinValue, LobbySizeConstants.MaxValue);
 
         var totalCount = countsByStatuses.Sum(x => x.Value);
 
@@ -34,16 +27,5 @@ public class TournamentPlatformStatsService(ITournamentsRepository tournamentsRe
             VerifiedByRuleset = countsByRulesets,
             VerifiedByLobbySize = countsByLobbySizes,
         };
-    }
-
-    private static void ImputeMissingKeys<T>(Dictionary<T, int> counts, T? minValue = null, T? maxValue = null) where T : struct, IConvertible
-    {
-        var start = (minValue ?? counts.Keys.Min()).ToInt32(null);
-        var end = (maxValue ?? counts.Keys.Max()).ToInt32(null);
-
-        for (var i = start; i <= end; i++)
-        {
-            counts.TryAdd((T)(object)i, 0);
-        }
     }
 }
