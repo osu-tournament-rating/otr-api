de72740e927aef6f0a03fbac429bb0c6028ef3b7
hburn7 <hburnett777@gmail.com>
2025-06-01 17:10:58 -0400

    Fix JSON deserialization bug

--- DataWorkerService/Services/Implementations/OsuApiDataParserService.cs
+++ DataWorkerService/Services/Implementations/OsuApiDataParserService.cs
@@ -92,7 +92,7 @@ public class OsuApiDataParserService(
                     CountGeki = mpScore.Statistics.CountGeki,
                     CountKatu = mpScore.Statistics.CountKatu,
                     Pass = mpScore.Passed,
-                    Perfect = mpScore.Perfect != 0,
+                    Perfect = mpScore.Perfect,
                     Grade = mpScore.Grade,
                     Mods = mpScore.Mods,
                     Ruleset = mpScore.Ruleset,
