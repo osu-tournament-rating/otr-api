264c62a8eb704843e5bd3797b8ec31fe340c47e0
hburn7 <hburnett777@gmail.com>
2025-06-18 00:16:46 -0400

    Add missing imports

--- API/Configurations/RateLimitConfiguration.cs
+++ API/Configurations/RateLimitConfiguration.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using JetBrains.Annotations;
 
 namespace API.Configurations;
 
--- DataWorkerService/Configurations/TournamentProcessingConfiguration.cs
+++ DataWorkerService/Configurations/TournamentProcessingConfiguration.cs
@@ -1,3 +1,5 @@
+using JetBrains.Annotations;
+
 namespace DataWorkerService.Configurations;
 
 /// <summary>
