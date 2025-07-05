730042e650c5e912a3c902019963ab1f9890cc50
hburn7 <hburnett777@gmail.com>
2025-07-05 12:07:29 -0400

    fix: Anonymous stat fetching + reduced cache lifetime

--- API/Controllers/PlatformStatsController.cs
+++ API/Controllers/PlatformStatsController.cs
@@ -13,14 +13,14 @@ namespace API.Controllers;
 [Route("api/v{version:apiVersion}/stats")]
 public class PlatformStatsController(IPlatformStatsService statsService) : Controller
 {
-    private const int StatsCacheDurationSeconds = 600;
+    private const int StatsCacheDurationSeconds = 10;
 
     /// <summary>
     /// Get various platform-wide stats
     /// </summary>
     /// <response code="200">Returns various platform-wide stats</response>
     [HttpGet]
-    [Authorize(Roles = OtrClaims.Roles.User)] // TODO: allow anonymous with anti-csrf protection
+    [AllowAnonymous]
     [ProducesResponseType<PlatformStatsDTO>(StatusCodes.Status200OK)]
     [OutputCache(Duration = StatsCacheDurationSeconds)]
     public async Task<IActionResult> GetAsync() => Ok(await statsService.GetAsync());
